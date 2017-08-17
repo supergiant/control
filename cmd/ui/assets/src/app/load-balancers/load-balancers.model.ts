688b03414fbb427e16a810248b67c23ccbc91646
Jordan Rinke <me@jordanrinke.com>
2017-08-17 13:00:52 -0500

    added all generated schemas except apps, fixed another kube sub issue

--- cmd/ui/assets/src/app/load-balancers/load-balancers.model.ts
+++ cmd/ui/assets/src/app/load-balancers/load-balancers.model.ts
@@ -1,1 +1,49 @@
-export class LoadBalancersModel {}
+export class LoadBalancersModel {
+  model = {
+    "kube_name": "",
+    "name": "",
+    "namespace": "default",
+    "ports": {
+      "80": 8080
+    },
+    "selector": {
+      "key": "value"
+    }
+  }
+  schema = {
+  "properties": {
+    "kube_name": {
+      "description": "Kube Name",
+      "type": "string"
+    },
+    "name": {
+      "description": "Name",
+      "type": "string"
+    },
+    "namespace": {
+      "default": "default",
+      "description": "Namespace",
+      "type": "string"
+    },
+    "ports": {
+      "properties": {
+        "80": {
+          "description": "80",
+          "type": "string"
+        }
+      },
+      "type": "object"
+    },
+    "selector": {
+      "properties": {
+        "key": {
+          "default": "value",
+          "description": "Key",
+          "type": "string"
+        }
+      },
+      "type": "object"
+    }
+  }
+}
+}
