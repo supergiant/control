b9023c438afc1e05bcb99bff844ffd854dd65166
Jordan Rinke <me@jordanrinke.com>
2017-11-08 17:41:19 -0600

    Added the basic clusters, cluster and, details component. None have any functionality yet, just sorting the routing out and determing how to proceed without making breaking changes as new functionality is shimmed in. EOB WIP.

---
+++ cmd/ui/assets/src/app/clusters/cluster-details/cluster-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cluster-details',
+  templateUrl: './cluster-details.component.html',
+  styleUrls: ['./cluster-details.component.css']
+})
+export class ClusterDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
