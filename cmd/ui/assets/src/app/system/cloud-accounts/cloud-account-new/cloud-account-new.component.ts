f856881ec444addc6c3e5a3980def93b3e75e1ec
Jordan Rinke <me@jordanrinke.com>
2017-11-09 12:01:54 -0600

    More structure setup

---
+++ cmd/ui/assets/src/app/system/cloud-accounts/cloud-account-new/cloud-account-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cloud-account-new',
+  templateUrl: './cloud-account-new.component.html',
+  styleUrls: ['./cloud-account-new.component.css']
+})
+export class CloudAccountNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
