93443607616c16d8612d2522c24aeacf5357cdd6
Serhii Aheienko <serhii.aheienko@gmail.com>
2018-05-15 17:56:57 +0300

    Add simple checks for go code (gofmt, gometalinter). (#485)
    * Add simple checks for go code (gofmt, gometalinter).
    
    * Add goimports check to Makefile
    
    * Add the govendor package to the get-tools target of the Makefile

---
+++ build/verify/goimports.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GOIMPORTS="goimports"
+bad_files=$(${GOIMPORTS} ${FLAGS:--l -local github.com/supergiant/supergiant} ./pkg ./cmd/ ./test)
+
+if [[ -n "${bad_files}" ]]; then
+  echo "!!! '$GOIMPORTS' needs to be run on the following files: "
+  echo "${bad_files}"
+  exit 1
+fi
+
