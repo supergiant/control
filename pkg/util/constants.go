84f9cf3aa896812dad55f2c8f7c02ef1c847433b
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-05-21 13:57:14 +0300

    Code clean up

--- pkg/util/constants.go
+++ pkg/util/constants.go
@@ -1,23 +1,6 @@
 package util
 
-import "time"
-
 const (
-	DefaultTimeout = 3 * time.Second
-
+	//UbuntuSlug1604 is used by digital ocean API to tell it to provision a droplet with Ubuntu 16.04 LTS x64
 	UbuntuSlug1604 = "ubuntu-16-04-x64"
-	K8SVersion1102 = "1.10.2"
-	K8SVersion187  = "1.8.7"
-	K8SVersion177  = "1.7.7"
-	K8SVersion167  = "1.6.7"
-	K8SVersion157  = "1.5.7"
-)
-
-type ProviderName string
-
-const (
-	DigitalOcean      ProviderName = "digitalocean"
-	Amazon            ProviderName = "aws"
-	GoogleCloudEngine ProviderName = "gce"
-	Packet            ProviderName = "packet"
 )
