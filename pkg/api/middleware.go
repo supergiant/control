d96cdbda576f8108d8c0f81be30a35daa52ba1a2
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-05-22 19:47:28 +0300

    Moving cloud accounts storage to the etc, storage layer. (#499)
    * Added domain and storage layer for cloud accounts
    * Added dependencies for the storage layer
    * Added basic middleware and user package

---
+++ pkg/api/middleware.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"net/http"
+	"regexp"
+
+	"github.com/supergiant/supergiant/pkg/user"
+)
+
+type middleware struct {
+	Users user.Service
+}
+
+//FIXME Reusing the old code to keep frontend from being broken
+func (m *middleware) authorisationHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		tokenMatch := regexp.MustCompile(`^SGAPI (token|session)="([A-Za-z0-9]{32})"$`).FindStringSubmatch(auth)
+
+		if len(tokenMatch) != 3 {
+			respond(rw, nil, errorBadAuthHeader)
+		}
+		switch tokenMatch[1] {
+		case "token":
+			token := tokenMatch[2]
+			_, err := m.Users.GetByToken(r.Context(), token)
+			if err != nil {
+				respond(rw, nil, errorUnauthorized)
+				return
+			}
+			//return user
+		case "session":
+			session := tokenMatch[3]
+			if err := m.Users.GetBySession(r.Context(), session); err != nil {
+				respond(rw, nil, errorUnauthorized)
+				return
+			}
+			return
+		default:
+			respond(rw, nil, errorBadAuthHeader)
+			return
+		}
+		next.ServeHTTP(rw, r)
+	})
+}
---
+++ pkg/cloud_account/service.go
@@ -0,0 +1,30 @@
+package cloud_account
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+)
+
+type Service struct {
+	Repository Repository
+}
+
+func (s *Service) GetAll(ctx context.Context) ([]CloudAccount, error) {
+	return s.Repository.GetAll(ctx)
+}
+
+//TODO add validations
+func (s *Service) Get(ctx context.Context, accountName string) (*CloudAccount, error) {
+	if accountName == "" {
+		return nil, errors.New("cloud account name can't be empty")
+	}
+	return s.Repository.Get(ctx, accountName)
+}
+
+func (s *Service) Create(ctx context.Context, account *CloudAccount) (error) {
+	if account.Name == "" {
+		return errors.New("cloud account name can't be empty")
+	}
+	return s.Repository.Create(ctx, account)
+}
