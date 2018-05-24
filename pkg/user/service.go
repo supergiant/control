d2c2b5e3915ac48e94533bddc87eae1f92f56c20
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-05-24 16:24:18 +0300

    Moved to etcd3 API, added generic data layer

--- pkg/user/service.go
+++ pkg/user/service.go
@@ -1,27 +1,42 @@
 package user
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/supergiant/supergiant/pkg/storage"
 	"golang.org/x/crypto/bcrypt"
 )
 
+const prefix = "/user/"
+
 // Service contains business logic related to users
 type Service struct {
-	Repository Repository
+	Repository storage.Interface
 }
 
 func (s *Service) GetByToken(ctx context.Context, apiToken string) (*User, error) {
-	users, err := s.Repository.GetAll(ctx)
+	logrus.Debug("user.Service.GetByToken start")
+	result, err := s.Repository.GetAll(ctx, prefix)
 	if err != nil {
 		return nil, err
 	}
-	for _, u := range users {
+	for _, rawUser := range result {
+		u := new(User)
+		err = json.NewDecoder(bytes.NewReader(rawUser)).Decode(u)
+		if err != nil {
+			logrus.Warningf("failed to convert stored data to cloud account struct")
+			logrus.Debugf("corrupted data: %s", rawUser)
+			continue
+		}
 		if u.APIToken == apiToken {
-			return &u, nil
+			return u, nil
 		}
 	}
+	logrus.Debug("user.Service.GetByToken end")
 	return nil, errors.Errorf("user with api token %s not found", apiToken)
 }
 
