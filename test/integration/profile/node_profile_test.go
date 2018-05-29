06953f9bb0c6a17cbaaba135fce20f0ed2088d23
stgleb <glebstepanov1992@gmail.com>
2018-05-29 15:18:20 +0300

    Add integration tests for profiles

---
+++ test/integration/profile/node_profile_test.go
@@ -0,0 +1,21 @@
+// +build integration
+
+package profile
+
+import "testing"
+
+func TestNodeProfileGet(t *testing.T) {
+
+}
+
+func TestNodeProfileCreate(t *testing.T) {
+
+}
+
+func TestNodeProfileGetAll(t *testing.T) {
+
+}
+
+func TestNodeProfileDelete(t *testing.T) {
+
+}
