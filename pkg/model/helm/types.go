6c0dd6052a7eca8714b3d4b9f214aebf7b64bed4
Serhii Aheienko <serhii.aheienko@gmail.com>
2018-05-30 11:40:54 +0300

    Update comments

--- pkg/model/helm/types.go
+++ pkg/model/helm/types.go
@@ -1,20 +1,24 @@
 package helm
 
+// Chart is representation of a helm chart.
 type Chart struct {
-	Name string
-	URL  string
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
+// ChartList stores a list of charts.
 type ChartList struct {
-	Items []Chart
+	Items []Chart `json:"items"`
 }
 
+// Repository is representation of a helm repository.
 type Repository struct {
-	Name   string
-	URL    string
-	Charts []Chart
+	Name   string  `json:"name"`
+	URL    string  `json:"url"`
+	Charts []Chart `json:"charts"`
 }
 
+// Repository stores a list of repositories.
 type RepositoryList struct {
-	Items []Repository
+	Items []Repository `json:"items"`
 }
