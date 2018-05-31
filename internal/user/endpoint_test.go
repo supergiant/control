7aef03d36daa35a2ce9e21e3a9686bc5068d746b
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-05-31 14:05:13 +0300

    Refactoring of SG API.  (#509)
    * Added basic kube service methods.
    * Fixed middleware test
    * Added API for CloudAccounts with tests
    * Goimports
    * Tests & comments for accounts
    * Moved SG internal code to the corresponding directory
    * Added more tests
    * Fixed errors

---
+++ internal/user/endpoint_test.go
@@ -0,0 +1,116 @@
+package user
+
+import (
+	"testing"
+	"net/http"
+	"github.com/supergiant/supergiant/pkg/jwt"
+	"bytes"
+	"net/http/httptest"
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+	"github.com/supergiant/supergiant/internal/testutils"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestEndpoint_Authenticate(t *testing.T) {
+	ts := jwt.NewTokenService(60, []byte("secret"))
+	testCases := []struct {
+		user         *User
+		ar           authRequest
+		expectedCode int
+	}{
+		{
+			&User{
+				Login:    "user1",
+				Password: "1234",
+			},
+			authRequest{
+				UserName: "user1",
+				Password: "1234",
+			},
+			http.StatusOK,
+		},
+		{
+			&User{
+				Login:    "user2",
+				Password: "1234",
+			},
+			authRequest{
+				UserName: "user1",
+				Password: "12345",
+			},
+			http.StatusForbidden,
+		},
+	}
+	storage := new(testutils.MockStorage)
+
+	userEndpoint := &Endpoint{
+		tokenService: ts,
+		userService:  Service{Repository: storage},
+	}
+	handler := http.HandlerFunc(userEndpoint.Authenticate)
+
+	for _, testCase := range testCases {
+		err := testCase.user.encryptPassword()
+		require.NoError(t, err)
+
+		buf := bytes.NewBuffer([]byte(""))
+		json.NewEncoder(buf).Encode(testCase.ar)
+
+		req, err := http.NewRequest("", "", buf)
+		require.NoError(t, err)
+
+		storage.On("Get", mock.Anything,
+			mock.Anything,
+			mock.Anything).Return(userToJSON(testCase.user), nil)
+
+		rec := httptest.NewRecorder()
+		handler.ServeHTTP(rec, req)
+
+		require.Equal(t, testCase.expectedCode, rec.Code)
+	}
+}
+
+func userToJSON(user *User) (data []byte) {
+	data, _ = json.Marshal(user)
+	return
+}
+
+func TestEndpoint_Create(t *testing.T) {
+	tt := []struct {
+		user         *User
+		expectedCode int
+	}{
+		{
+			user: &User{
+				Login:    "login",
+				Password: "password",
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			user: &User{
+				Login:    "",
+				Password: "",
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+	}
+	storage := new(testutils.MockStorage)
+	userEndpoint := &Endpoint{
+		userService: Service{Repository: storage},
+	}
+	handler := http.HandlerFunc(userEndpoint.Create)
+
+	storage.On("Put", mock.Anything, mock.Anything,
+		mock.Anything, mock.Anything).Return(nil)
+
+	for _, testCase := range tt {
+		req, err := http.NewRequest("", "", bytes.NewReader(userToJSON(testCase.user)))
+		require.NoError(t, err)
+
+		rec := httptest.NewRecorder()
+		handler.ServeHTTP(rec, req)
+		require.Equal(t, testCase.expectedCode, rec.Code)
+	}
+}
