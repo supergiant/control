b68d54ab17562a481939dae26abe5272a8ae7d17
Gleb <glebstepanov1992@gmail.com>
2018-06-01 14:45:47 +0300

    Add integration tests for different cloud providers (#481)
    * Add tests for different versions of k8s and cloud providers
    * Add integration tests on AWS using support user
    * Run server once
    * AWS integration tests work
    * Add test for Digital Ocean
    * Digital ocean tests work
    * Work on GCE integration tests
    * Finish GCE tests
    * Finish integration tests
    * Add short mode for integration tests
    * Add sleep to wait for resource be freed
    enable integration tests run in parallel
    for different cloud providers.
    * Update according to comments
    * Run tests in parallel
    * Make errors more informative
    * Add build tag for integration tests

---
+++ test/integration/providers/digital_ocean_test.go
@@ -0,0 +1,86 @@
+// +build integration
+
+package providers
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/supergiant/supergiant/pkg/util"
+)
+
+func TestDigitalOcean(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skip integration tests for short mode")
+	}
+
+	// Run test in parallel for different cloud providers
+	t.Parallel()
+
+	region := os.Getenv("DO_REGION")
+	fingerPrint := os.Getenv("DO_KEY_FINGER_PRINT")
+	token := os.Getenv("DO_TOKEN")
+
+	k8sVersions := []string{"1.8.7", "1.7.7", "1.6.7", "1.5.7"}
+	srv, err := newServer()
+
+	if err != nil {
+		t.Errorf("Unexpected error while creating client and server %v", err)
+		return
+	}
+
+	requestor := createAdmin(srv.Core)
+	client := srv.Core.APIClient("token", requestor.APIToken)
+
+	go srv.Start()
+	defer srv.Stop()
+
+	cloudAccount, err := createCloudAccount(client, map[string]string{"token": token}, "digitalocean")
+
+	if err != nil {
+		t.Errorf("Unexpected error while creating cloud account %v", err)
+		return
+	}
+
+	for _, k8sVersion := range k8sVersions {
+		t.Run(fmt.Sprintf("Test Digital Ocean k8s version %s", k8sVersion), func(t *testing.T) {
+			kube, err := createKubeDO(client,
+				cloudAccount,
+				fmt.Sprintf("test-%s", strings.ToLower(util.RandomString(5))),
+				region,
+				fingerPrint,
+				k8sVersion)
+
+			if err != nil {
+				t.Errorf("Unexpected error while creating a kube %v", err)
+				return
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*600)
+			defer cancel()
+
+			err = util.WaitFor(ctx, "Wait for cluster to start", time.Second*30, func() (bool, error) {
+				err := client.Kubes.Get(kube.ID, kube)
+
+				if err != nil {
+					return false, err
+				}
+
+				return kube.Ready, nil
+			})
+
+			// Clean up afterwards
+			err = client.Kubes.Delete(kube.ID, kube)
+
+			if err != nil {
+				t.Errorf("Error while deleting the kube %d", kube.ID)
+			}
+
+			time.Sleep(time.Minute)
+		})
+	}
+}
