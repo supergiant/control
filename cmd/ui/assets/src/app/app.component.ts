10686728fb0cc5c01ebc7d0113a3ec0d547155da
josephkane <josephckane@gmail.com>
2018-06-08 14:45:25 -0500

    WIP remove header from login and logout pages

--- cmd/ui/assets/src/app/app.component.ts
+++ cmd/ui/assets/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Event, NavigationEnd } from '@angular/router';
 
 
 
@@ -11,13 +11,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AppComponent {
   title = 'app';
+  public showHeader = true;
   public location = '';
   public options = {
     position: ['top', 'left'],
     timeOut: 2000,
     lastOnBottom: true,
   };
-  constructor(private _router: ActivatedRoute) {
+  constructor(private _router: ActivatedRoute, router: Router) {
     this.location = _router.snapshot.url.join('');
+    router.events.subscribe((e: Event) => {
+      if (e instanceof NavigationEnd) {
+        if (e.urlAfterRedirects === "/" || e.urlAfterRedirects === "/#log-out") {
+          this.showHeader = false;
+        } else {
+          this.showHeader = true;
+        }
+      }
+    })
   }
 }
