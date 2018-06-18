631f833727b04bc6f093fef01ad97221263accc7
stgleb <glebstepanov1992@gmail.com>
2018-06-18 16:59:08 +0300

    Remove unrelated changes

--- scripts/install_tiller.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget http://storage.googleapis.com/kubernetes-helm/helm-v2.8.2-linux-amd64.tar.gz --directory-prefix=/tmp/
-tar -C /tmp -xvf /tmp/helm-v2.8.2-linux-amd64.tar.gz
-cp /tmp/linux-amd64/helm /opt/bin/helm
-chmod +x /opt/bin/helm
-/opt/bin/helm init
