ce7d1f5f170b80918bc81383702a2c01b319ba42
stgleb <glebstepanov1992@gmail.com>
2018-06-19 14:40:59 +0300

    Make sshRunner correspond to runner interface

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -4,5 +4,5 @@ import "github.com/supergiant/supergiant/pkg/runner/command"
 
 // Runner is interface for running command in different environment
 type Runner interface {
-	Run(command *command.Command)
+	Run(command *command.Command) error
 }
