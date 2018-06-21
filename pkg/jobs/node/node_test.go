e78c2b65e43fc8022ed2c55dd6b7020a2af13a77
stgleb <glebstepanov1992@gmail.com>
2018-06-21 14:30:02 +0300

    Add tests for negativ case

--- pkg/jobs/node/node_test.go
+++ pkg/jobs/node/node_test.go
@@ -7,13 +7,19 @@ import (
 	"testing"
 	"text/template"
 
+	"github.com/pkg/errors"
 	"github.com/supergiant/supergiant/pkg/runner"
 )
 
 type fakeRunner struct {
+	errMsg string
 }
 
 func (f *fakeRunner) Run(command *runner.Command) error {
+	if len(f.errMsg) > 0 {
+		return errors.New(f.errMsg)
+	}
+
 	_, err := io.Copy(command.Out, strings.NewReader(command.Script))
 	return err
 }
@@ -54,9 +60,9 @@ func TestJob_ProvisionNode(t *testing.T) {
 
 	cfg := JobConfig{
 		KubernetesVersion: k8sVersion,
-		MasterPrivateIP: masterIp,
-		ProxyPort: proxyPort,
-		EtcdPort: etcdPort,
+		MasterPrivateIP:   masterIp,
+		ProxyPort:         proxyPort,
+		EtcdPort:          etcdPort,
 	}
 
 	err = j.ProvisionNode(cfg)
@@ -71,5 +77,38 @@ func TestJob_ProvisionNode(t *testing.T) {
 
 	if !strings.Contains(output.String(), k8sVersion) {
 		t.Errorf("k8s version %s not found in %s", k8sVersion, output.String())
+	}
+}
+
+func TestJob_ProvisionNodeError(t *testing.T) {
+	errMsg := "error has occurred"
+
+	r := &fakeRunner{
+		errMsg: errMsg,
+	}
+
+	kubeletScriptTemplate, err := template.New("kubelet").Parse("")
+	proxyTemplate, err := template.New("proxy").Parse("")
+
+	output := new(bytes.Buffer)
+
+	j := &Job{
+		r,
+		kubeletScriptTemplate,
+		proxyTemplate,
+		output,
+		output,
+	}
+
+	cfg := JobConfig{}
+	err = j.ProvisionNode(cfg)
+
+	if err == nil {
+		t.Errorf("Error must not be nil")
+		return
+	}
+
+	if !strings.Contains(err.Error(), errMsg) {
+		t.Errorf("Error message expected to contain %s actual %s", errMsg, err.Error())
 	}
 }
