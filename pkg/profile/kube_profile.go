2f0a538871951e5c7f5fac1c2e7c6102b1b8fdc1
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-06-25 14:10:38 +0300

    Fixed json tags for kube profile

--- pkg/profile/kube_profile.go
+++ pkg/profile/kube_profile.go
@@ -3,9 +3,9 @@ package profile
 type KubeProfile struct {
 	Id                string      `json:"id" valid:"required"`
 	KubernetesVersion string      `json:"kubernetes_version" valid:"required"`
-	MasterProfile     NodeProfile `json:"master_profile" valid:"required"`
-	NodesProfile      NodeProfile `json:"nodes_profile" valid:"required"`
+	MasterProfile     NodeProfile `json:"masterProfile" valid:"required"`
+	NodesProfile      NodeProfile `json:"nodesProfile" valid:"required"`
 	MasterNodeCount   int         `json:"master_node_count" valid:"required"`
-	CustomFiles       string      `json:"custom_files" valid:"optional"`
-	RBACEnabled       bool        `json:"rbac_enabled"`
+	CustomFiles       string      `json:"customFiles" valid:"optional"`
+	RBACEnabled       bool        `json:"rbacEnabled"`
 }
