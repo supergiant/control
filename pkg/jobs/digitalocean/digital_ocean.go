9873d00018cd4fb5cfa70dc9bb88771c169d44e3
stgleb <glebstepanov1992@gmail.com>
2018-06-25 19:01:04 +0300

    [WIP] Add droplet create task

--- pkg/jobs/digitalocean/digital_ocean.go
+++ pkg/jobs/digitalocean/digital_ocean.go
@@ -1,21 +1,20 @@
 package digitalocean
 
 import (
-	"strings"
-	"github.com/supergiant/supergiant/pkg/util"
-	"bytes"
-	"github.com/digitalocean/godo"
 	"strconv"
-	"time"
+	"strings"
+
+	"golang.org/x/oauth2"
+
+	"github.com/digitalocean/godo"
+	"github.com/supergiant/supergiant/pkg/util"
 )
 
-type Job struct{
-
+type Job struct {
 }
 
-func (j *Job) CreateDroplet(name, k8sVersion, region, size string, fingerprints []string) error {
+func (j *Job) CreateDroplet(credentials map[string]string, name, k8sVersion, region, size string, fingerprints []string) error {
 	name = name + "-master" + "-" + strings.ToLower(util.RandomString(5))
-	mversion := strings.Split(k8sVersion, ".")
 
 	var fingers []godo.DropletCreateSSHKey
 	for _, ssh := range fingerprints {
@@ -34,32 +33,72 @@ func (j *Job) CreateDroplet(name, k8sVersion, region, size string, fingerprints 
 			Slug: "ubuntu-stable",
 		},
 	}
-	tags := []string{"Kubernetes-Cluster", name}
 
-	masterDroplet, _, err := p.createDroplet(client, action, dropletRequest, tags)
+	client := getClient(credentials)
+	tags := []string{"Kubernetes-Cluster", name}
+	droplet, err := createDroplet(client, dropletRequest, tags)
+
 	if err != nil {
 		return err
 	}
 
-	master := strconv.Itoa(masterDroplet.ID)
-	m.MasterID = master
+	for {
+		droplet, _, err = client.Droplets.Get(droplet.ID)
 
-	return action.CancellableWaitFor("Kubernetes master launch", 10*time.Minute, 3*time.Second, func() (bool, error) {
-		resp, _, serr := client.Droplets.Get(masterDroplet.ID)
-		if serr != nil {
-			return false, serr
+		if err != nil {
+			return err
 		}
 
-		// Save Master info when ready
-		if resp.Status == "active" {
-			m.MasterNodes = append(m.MasterNodes, strconv.Itoa(resp.ID))
-			m.MasterPrivateIP, _ = resp.PrivateIPv4()
-			m.MasterPublicIP, _ = resp.PublicIPv4()
-			if serr := p.Core.DB.Save(m); serr != nil {
-				return false, serr
-			}
+		if droplet.Status == "active" {
+			//TODO(stgleb): Save droplet related data to Etcd
+			privateIP, _ := droplet.PrivateIPv4()
+			publicIP, _ := droplet.PublicIPv4()
+			return nil
 		}
-		return resp.Status == "active", nil
-	})
-})
+	}
+
+	return nil
+}
+
+func createDroplet(client *godo.Client, req *godo.DropletCreateRequest, tags []string) (droplet *godo.Droplet, err error) {
+	// Create
+	droplet, _, err = client.Droplets.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, tag := range tags {
+		input := &godo.TagResourcesRequest{
+			Resources: []godo.Resource{
+				{
+					ID:   strconv.Itoa(droplet.ID),
+					Type: godo.DropletResourceType,
+				},
+			},
+		}
+		if _, err = client.Tags.TagResources(tag, input); err != nil {
+			return nil, err
+		}
+	}
+
+	return droplet, nil
+}
+
+type TokenSource struct {
+	AccessToken string
+}
+
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
+
+func getClient(credentials map[string]string) *godo.Client {
+	token := &TokenSource{
+		AccessToken: credentials["token"],
+	}
+	oauthClient := oauth2.NewClient(oauth2.NoContext, token)
+	return godo.NewClient(oauthClient)
 }
