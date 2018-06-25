89a541b2e48e5c20642f132b0bf2d806e481f9b1
josephkane <josephckane@gmail.com>
2018-06-25 10:19:56 -0500

    Format new-cluster UI
    This commit updates the formatting of all new-cluster forms.
    Regions are now chosen via dropdowns, the selected cloud account is
    removed from the form, and some validation has been added. Additionally,
    formatting is done throught 'layout' key in the cluster model.

--- cmd/ui/assets/src/app/clusters/cluster.aws.model.ts
+++ cmd/ui/assets/src/app/clusters/cluster.aws.model.ts
@@ -21,63 +21,120 @@ export class ClusterAWSModel {
     'schema': {
       'properties': {
         'name': {
-          'description': 'The desired name of the kube. Max length of 12 characters.',
           'type': 'string',
+          'description': 'The desired name of the kube. Max length of 12 characters.',
           'pattern': '^[a-z]([-a-z0-9]*[a-z0-9])?$',
           'maxLength': 12
         },
         'kubernetes_version': {
+          'type': 'string',
           'default': '1.5.7',
           'description': 'The Version of Kubernetes to be deployed.',
-          'type': 'string',
           'enum': ['1.5.7', '1.6.7', '1.7.7', '1.8.7'] // TODO: <-- Should be dynamically populated.
         },
-        'cloud_account_name': {
-          'description': 'The Supergiant cloud account you created for use with AWS.',
-          'type': 'string'
+        'cloud_account_name': { // TODO: <-- Should be dynamically populated enum with any cloud accounts linked to provider
+          'type': 'string',
+          'description': 'The Supergiant cloud account you created for use with AWS.'
         },
         'aws_config': {
+          'type': 'object',
           'properties': {
             'region': {
-              'default': 'us-east-1',
+              'type': 'string',
               'description': 'The AWS region the kube will be created in.',
-              'type': 'string'
+              'default': 'us-east-1',
+              'enum': [
+                'us-east-1',
+                'us-east-2',
+                'us-west-1',
+                'us-west-2',
+                'ap-northeast-1',
+                'ap-northeast-2',
+                'ap-northeast-3',
+                'ap-south-1',
+                'ap-southeast-1',
+                'ap-southeast-2',
+                'ca-central-1',
+                'cn-north-1',
+                'cn-northwest-1',
+                'eu-central-1',
+                'eu-west-1',
+                'eu-west-2',
+                'eu-west-3',
+                'sa-east-1'
+              ] // TODO: <-- Should be dynamically populated.
             },
             'vpc_ip_range': {
               'default': '172.20.0.0/16',
               'description': 'The range of IP addresses you want available to the kube.',
               'type': 'string'
             }
-          },
-          'type': 'object'
+          }
         },
         'ssh_pub_key': {
-          'description': 'The public key that will be used to SSH into the kube.',
           'type': 'string',
-          'widget': 'textarea',
+          'description': 'The public key that will be used to SSH into the kube.',
+          'widget': 'textarea'
         },
         'master_node_size': {
+          'type': 'string',
           'default': 'm4.large',
-          'description': 'The size of the server the master will live on.',
-          'type': 'string'
+          'description': 'The size of the server the master will live on.'
         },
         'kube_master_count': {
-          'description': 'The number of masters desired--for High Availability.',
           'type': 'number',
+          'description': 'The number of masters desired - for High Availability.',
           'widget': 'number',
+          'minimum': 1
         },
         'node_sizes': {
+          'type': 'array',
           'description': 'The sizes you want to be available to Supergiant when scaling.',
           'widget': 'array',
           'items': {
             'type': 'string'
-          },
-          'type': 'array'
+          }
         }
-      }
+      },
+      'required': [ 'name' ]
     },
     'layout': [
-      { 'type': 'flex', 'flex-flow': 'row wrap', 'items': ['name', 'region'] },
+      {
+        'type': 'section',
+        'title': 'Cluster Details',
+        'items': [
+          { 'key': 'name' },
+          { 'key': 'kubernetes_version' },
+          // { 'key': 'cloud_account_name', 'readonly': true }
+        ]
+      },
+      {
+        'type': 'section',
+        'title': 'AWS Config',
+        'items': [
+          { 'key': 'aws_config.region' },
+          { 'key': 'aws_config.vpc_ip_range' }
+        ]
+      },
+      {
+        'type': 'section',
+        'items': [
+          { 'key': 'ssh_pub_key' },
+          { 'key': 'master_node_size' },
+          { 'key': 'kube_master_count' },
+        ]
+      },
+      {
+        'key': 'node_sizes',
+        'type': 'array',
+        'items': [
+          { 'key': 'node_sizes[]' },
+        ]
+      },
+      {
+        'type': 'submit',
+        'title': 'Create'
+      }
     ]
   };
 }
--- cmd/ui/assets/src/app/clusters/cluster.digitalocean.model.ts
+++ cmd/ui/assets/src/app/clusters/cluster.digitalocean.model.ts
@@ -4,9 +4,10 @@ export class ClusterDigitalOceanModel {
       'cloud_account_name': '',
       'digitalocean_config': {
         'region': 'nyc1',
-        'kube_master_count': 1,
         'ssh_key_fingerprint': []
       },
+      'kubernetes_version': '1.8.7',
+      'kube_master_count': 1,
       'master_node_size': '1gb',
       'name': '',
       'node_sizes': [
@@ -23,60 +24,121 @@ export class ClusterDigitalOceanModel {
     'schema': {
       'properties': {
         'name': {
-          'description': 'The desired name of the kube. Max length of 12 characters.',
           'type': 'string',
+          'description': 'The desired name of the kube. Max length of 12 characters.',
           'pattern': '^[a-z]([-a-z0-9]*[a-z0-9])?$',
           'maxLength': 12
         },
         'kubernetes_version': {
-          'default': '1.5.7',
-          'description': 'The Version of Kubernetes to be deployed.',
           'type': 'string',
+          'description': 'The Version of Kubernetes to be deployed.',
+          'default': '1.5.7',
           'enum': ['1.5.7', '1.6.7', '1.7.7', '1.8.7'] // TODO: <-- Should be dynamically populated.
         },
         'cloud_account_name': {
-          'description': 'The Supergiant cloud account you created for use with Packet.',
-          'type': 'string'
+          'type': 'string',
+          'description': 'The Supergiant cloud account you created for use with Digital Ocean.'
         },
         'digitalocean_config': {
+          'type': 'object',
           'properties': {
             'region': {
-              'default': 'nyc1',
+              'type': 'string',
               'description': 'The Digital Ocean region the kube will be created in.',
-              'type': 'string'
+              'default': 'nyc1',
+              'enum': [
+                'ams2',
+                'ams3',
+                'blr1',
+                'fra1',
+                'lon1',
+                'nyc1',
+                'nyc2',
+                'nyc3',
+                'sfo1',
+                'sfo2',
+                'sgp1',
+                'tor1'
+              ]
             },
             'ssh_key_fingerprint': {
-              'description': 'The fingerprint of the public key that you uploaded to your OpenStack account.',
+              'type': 'array',
+              'description': 'The fingerprint of the public key that you uploaded to your Digital Ocean account.',
               'id': '/properties/ssh_key_fingerprint',
               'items': {
-                'id': '/properties/ssh_key_fingerprint/items',
-                'type': 'string'
-              },
-              'type': 'array'
+                'type': 'string',
+                'id': '/properties/ssh_key_fingerprint/items'
+              }
             }
           },
-          'type': 'object'
+          'required': [ 'region' ]
         },
         'master_node_size': {
-          'default': '1gb',
+          'type': 'string',
           'description': 'The size of the server the master will live on.',
-          'type': 'string'
+          'default': '1gb'
         },
         'kube_master_count': {
-          'description': 'The number of masters desired--for High Availability.',
           'type': 'number',
+          'description': 'The number of masters desired - for High Availability.',
           'widget': 'number',
+          'minimum': 1
         },
         'node_sizes': {
+          'type': 'array',
           'description': 'The sizes you want to be available to Supergiant when scaling.',
           'id': '/properties/node_sizes',
           'items': {
-            'id': '/properties/node_sizes/items',
-            'type': 'string'
-          },
-          'type': 'array'
+            'type': 'string',
+            'id': '/properties/node_sizes/items'
+          }
         }
+      },
+      'required': [ 'name' ]
+    },
+    'layout': [
+      {
+        'type': 'section',
+        'title': 'Cluster Details',
+        'items': [
+          { 'key': 'name' },
+          { 'key': 'kubernetes_version' },
+          // { 'key': 'cloud_account_name', 'readonly': true }
+        ]
+      },
+      {
+        'type': 'section',
+        'title': 'Digital Ocean Config',
+        'items': [
+          { 'key': 'digitalocean_config.region' },
+          {
+            'key': 'digitalocean_config.ssh_key_fingerprint',
+            'type': 'array',
+            'title': 'SSH Key Fingerprint',
+            'items': [
+              { 'key': 'digitalocean_config.ssh_key_fingerprint[]' }
+            ]
+          } // TODO: can you really add multiple fingerprints?
+        ]
+      },
+      {
+        'type': 'section',
+        'items': [
+          { 'key': 'master_node_size' },
+          { 'key': 'kube_master_count' },
+        ]
+      },
+      {
+        'key': 'node_sizes',
+        'type': 'array',
+        'items': [
+          { 'key': 'node_sizes[]' },
+        ]
+      },
+      {
+        'type': 'submit',
+        'title': 'Create'
       }
-    }
+    ]
   };
 }
