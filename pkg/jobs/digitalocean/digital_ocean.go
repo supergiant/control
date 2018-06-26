ced91c54934bc332101a8553c291c687ecddd0d4
stgleb <glebstepanov1992@gmail.com>
2018-06-26 16:35:02 +0300

    Add digitalocean task for create node

--- pkg/jobs/digitalocean/digital_ocean.go
+++ pkg/jobs/digitalocean/digital_ocean.go
@@ -1,32 +1,80 @@
 package digitalocean
 
 import (
+	"context"
+	"encoding/json"
 	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/oauth2"
 
 	"github.com/digitalocean/godo"
+	"github.com/pkg/errors"
+
+	"github.com/coreos/etcd/clientv3"
+
+	"github.com/supergiant/supergiant/pkg/storage"
 	"github.com/supergiant/supergiant/pkg/util"
 )
 
-type Job struct {
+var (
+	// TODO(stgleb): We need global error for timeout exceeding
+	ErrTimeoutExceeded = errors.New("timeout exceeded")
+)
+
+type DropletService interface {
+	Get(int) (*godo.Droplet, *godo.Response, error)
+	Create(*godo.DropletCreateRequest) (*godo.Droplet, *godo.Response, error)
 }
 
-func (j *Job) CreateDroplet(credentials map[string]string, name, k8sVersion, region, size string, fingerprints []string) error {
-	name = name + "-master" + "-" + strings.ToLower(util.RandomString(5))
+type TagService interface {
+	TagResources(string, *godo.TagResourcesRequest) (*godo.Response, error)
+}
+
+type Job struct {
+	storage        storage.Interface
+	dropletService DropletService
+	tagService     TagService
+}
+
+type Config struct {
+	Name         string
+	K8sVersion   string
+	Region       string
+	Size         string
+	Role         string // master/node
+	Fingerprints []string
+
+	DropletTimeout time.Duration
+	CheckPeriod    time.Duration
+}
+
+func NewJob(credentials map[string]string, cfg clientv3.Config) *Job {
+	c := getClient(credentials)
+	s := storage.NewETCDRepository(cfg)
+
+	return &Job{
+		storage:        s,
+		dropletService: c.Droplets,
+		tagService:     c.Tags,
+	}
+}
+
+func (j *Job) CreateDroplet(config Config) error {
+	config.Name = config.Name + "-" + config.Role + "-" + strings.ToLower(util.RandomString(5))
 
 	var fingers []godo.DropletCreateSSHKey
-	for _, ssh := range fingerprints {
+	for _, ssh := range config.Fingerprints {
 		fingers = append(fingers, godo.DropletCreateSSHKey{
 			Fingerprint: ssh,
 		})
 	}
 
 	dropletRequest := &godo.DropletCreateRequest{
-		Name:              name,
-		Region:            region,
-		Size:              size,
+		Name:              config.Name,
+		Region:            config.Region,
+		Size:              config.Size,
 		PrivateNetworking: true,
 		SSHKeys:           fingers,
 		Image: godo.DropletCreateImage{
@@ -34,54 +82,66 @@ func (j *Job) CreateDroplet(credentials map[string]string, name, k8sVersion, reg
 		},
 	}
 
-	client := getClient(credentials)
-	tags := []string{"Kubernetes-Cluster", name}
-	droplet, err := createDroplet(client, dropletRequest, tags)
+	tags := []string{"Kubernetes-Cluster", config.Name}
+
+	// Create
+	droplet, _, err := j.dropletService.Create(dropletRequest)
 
 	if err != nil {
 		return err
 	}
 
+	err = j.tagDroplet(droplet.ID, tags)
+
+	if err != nil {
+		return err
+	}
+
+	after := time.After(config.DropletTimeout)
+	ticker := time.NewTicker(config.CheckPeriod)
+
 	for {
-		droplet, _, err = client.Droplets.Get(droplet.ID)
+		select {
+		case <-ticker.C:
+			droplet, _, err = j.dropletService.Get(droplet.ID)
 
-		if err != nil {
-			return err
-		}
+			if err != nil {
+				return err
+			}
+			// Wait for droplet becomes active
+			if droplet.Status == "active" {
+				if data, err := json.Marshal(droplet); err != nil {
+					return err
+				} else {
+					return j.storage.Put(context.Background(), "droplet", droplet.Name, data)
+				}
+			}
 
-		if droplet.Status == "active" {
-			//TODO(stgleb): Save droplet related data to Etcd
-			privateIP, _ := droplet.PrivateIPv4()
-			publicIP, _ := droplet.PublicIPv4()
-			return nil
+		case <-after:
+			return ErrTimeoutExceeded
 		}
 	}
 
 	return nil
 }
 
-func createDroplet(client *godo.Client, req *godo.DropletCreateRequest, tags []string) (droplet *godo.Droplet, err error) {
-	// Create
-	droplet, _, err = client.Droplets.Create(req)
-	if err != nil {
-		return nil, err
-	}
-
+func (j *Job) tagDroplet(dropletId int, tags []string) error {
+	// Tag droplet
 	for _, tag := range tags {
 		input := &godo.TagResourcesRequest{
 			Resources: []godo.Resource{
 				{
-					ID:   strconv.Itoa(droplet.ID),
+					ID:   strconv.Itoa(dropletId),
 					Type: godo.DropletResourceType,
 				},
 			},
 		}
-		if _, err = client.Tags.TagResources(tag, input); err != nil {
-			return nil, err
+		if _, err := j.tagService.TagResources(tag, input); err != nil {
+			return err
 		}
 	}
 
-	return droplet, nil
+	return nil
 }
 
 type TokenSource struct {
---
+++ pkg/jobs/digitalocean/digital_ocean_test.go
@@ -0,0 +1,213 @@
+package digitalocean
+
+import (
+	"testing"
+	"time"
+
+	"github.com/digitalocean/godo"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/supergiant/supergiant/pkg/storage"
+	"github.com/supergiant/supergiant/pkg/testutils"
+)
+
+type createDropletResponse struct {
+	droplet *godo.Droplet
+	resp    *godo.Response
+	err     error
+}
+
+type getDropletResponse struct {
+	droplet *godo.Droplet
+	resp    *godo.Response
+	err     error
+}
+
+type (
+	fakeDropletService struct {
+		createCounter int
+		getCounter    int
+
+		createResponses []createDropletResponse
+		getResponses    []getDropletResponse
+	}
+	fakeTagService struct {
+		counter int
+		errs    []error
+	}
+)
+
+func (f *fakeDropletService) Get(id int) (*godo.Droplet, *godo.Response, error) {
+	if f.getCounter > len(f.getResponses)-1 {
+		panic("get index out of range")
+	}
+
+	r := f.getResponses[f.getCounter]
+	f.getCounter++
+	return r.droplet, r.resp, r.err
+}
+
+func (f *fakeDropletService) Create(req *godo.DropletCreateRequest) (*godo.Droplet, *godo.Response, error) {
+	if f.createCounter > len(f.createResponses)-1 {
+		panic("create index out of range")
+	}
+	r := f.createResponses[f.createCounter]
+	f.createCounter++
+	return r.droplet, r.resp, r.err
+}
+
+func (f *fakeTagService) TagResources(string, *godo.TagResourcesRequest) (*godo.Response, error) {
+	return nil, nil
+}
+
+func TestJob_CreateDropletSuccess(t *testing.T) {
+	dropletId := 1
+	m := new(testutils.MockStorage)
+	m.On("Put", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+	testCases := []struct {
+		storage        storage.Interface
+		dropletService *fakeDropletService
+		tagService     *fakeTagService
+
+		dropletTimeout time.Duration
+		checkTimeout   time.Duration
+
+		expectedError error
+	}{
+		{
+			storage: m,
+			dropletService: &fakeDropletService{
+				0,
+				0,
+				[]createDropletResponse{
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "pending",
+						},
+						nil,
+						nil,
+					},
+				},
+				[]getDropletResponse{
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "pending",
+						},
+						nil,
+						nil,
+					},
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "pending",
+						},
+						nil,
+						nil,
+					},
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "active",
+						},
+						nil,
+						nil,
+					},
+				},
+			},
+			tagService: &fakeTagService{
+				0,
+				[]error{},
+			},
+			dropletTimeout: time.Millisecond * 30,
+			checkTimeout:   time.Millisecond * 1,
+			expectedError:  nil,
+		},
+		{
+			storage: m,
+			dropletService: &fakeDropletService{
+				0,
+				0,
+				[]createDropletResponse{
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "pending",
+						},
+						nil,
+						nil,
+					},
+				},
+				[]getDropletResponse{
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "pending",
+						},
+						nil,
+						nil,
+					},
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "pending",
+						},
+						nil,
+						nil,
+					},
+					{
+						&godo.Droplet{
+							ID:     dropletId,
+							Name:   "test",
+							Status: "pending",
+						},
+						nil,
+						nil,
+					},
+				},
+			},
+			tagService: &fakeTagService{
+				0,
+				[]error{},
+			},
+			dropletTimeout: time.Millisecond * 2,
+			checkTimeout:   time.Millisecond * 1,
+			expectedError:  ErrTimeoutExceeded,
+		},
+	}
+
+	for _, testCase := range testCases {
+		job := &Job{
+			storage:        testCase.storage,
+			dropletService: testCase.dropletService,
+			tagService:     testCase.tagService,
+		}
+
+		config := Config{
+			"test",
+			"1.8.7",
+			"us-west1",
+			"2GB",
+			"master",
+			[]string{"fingerprint"},
+
+			testCase.dropletTimeout,
+			testCase.checkTimeout,
+		}
+
+		err := job.CreateDroplet(config)
+
+		if err != testCase.expectedError {
+			t.Errorf("Wrong error expected %s actual %s", testCase.expectedError.Error(), err.Error())
+		}
+	}
+}
