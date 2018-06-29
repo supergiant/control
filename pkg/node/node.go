13127e2890953fa65d9d5067d8a8d0cee4363aa7
stgleb <glebstepanov1992@gmail.com>
2018-06-29 15:37:05 +0300

    Add implementation for node service

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -1,9 +1,12 @@
 package node
 
+import "github.com/supergiant/supergiant/pkg/provider"
+
 type Node struct {
-	Id        string `json:"id"`
-	CreatedAt int64  `json:"created_at"`
-	Region    string `json:"region"`
-	PublicIp  string `json:"public_ip"`
-	PrivateIp string `json:"private_ip"`
+	Id        string        `json:"id"`
+	CreatedAt int64         `json:"created_at"`
+	Provider  provider.Name `json:"provider"`
+	Region    string        `json:"region"`
+	PublicIp  string        `json:"public_ip"`
+	PrivateIp string        `json:"private_ip"`
 }
