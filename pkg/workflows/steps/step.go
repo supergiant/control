65cf167864de9fbad6cfeac3d0f99fba083a29ff
stgleb <glebstepanov1992@gmail.com>
2018-07-24 14:42:47 +0300

    More fix on circular deps

--- pkg/workflows/steps/step.go
+++ pkg/workflows/steps/step.go
@@ -2,9 +2,8 @@ package steps
 
 import (
 	"context"
-	"github.com/supergiant/supergiant/pkg/workflows"
 )
 
 type Step interface {
-	Run(ctx context.Context, config workflows.Config) error
+	Run(ctx context.Context, config Config) error
 }
