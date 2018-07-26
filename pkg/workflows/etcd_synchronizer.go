e5727d65141146dea5996a279648037ebed3090d
stgleb <glebstepanov1992@gmail.com>
2018-07-26 17:20:11 +0300

    Add etcd for sync

---
+++ pkg/workflows/etcd_synchronizer.go
@@ -0,0 +1,33 @@
+package workflows
+
+import (
+	"github.com/coreos/etcd/clientv3"
+	"context"
+)
+
+type EtcdSynchronizer struct{
+	client *clientv3.Client
+}
+
+func NewSynchronizer(cfg clientv3.Config) (*EtcdSynchronizer, error) {
+	client, err := clientv3.New(cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &EtcdSynchronizer{
+		client: client,
+	}, nil
+}
+
+// TODO(stgleb): Pas context and key to synchronizer
+func (s *EtcdSynchronizer) Sync(data string) error {
+	_, err := s.client.Put(context.Background(), "workflow", data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
