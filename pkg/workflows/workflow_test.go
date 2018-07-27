93bd5e07666cb54088834467e97b2b126e606e22
stgleb <glebstepanov1992@gmail.com>
2018-07-27 17:24:16 +0300

    Add fake task type for tests

--- pkg/workflows/workflow_test.go
+++ pkg/workflows/workflow_test.go
@@ -3,7 +3,9 @@ package workflows
 import (
 	"bytes"
 	"context"
+	"github.com/pkg/errors"
 	"github.com/supergiant/supergiant/pkg/workflows/steps"
+	"io"
 	"testing"
 )
 
@@ -17,12 +19,34 @@ func (f *fakeSynchronizer) Sync(ctx context.Context, key, data string) error {
 	return nil
 }
 
+type fakeStep struct {
+	name        string
+	description string
+	err         error
+}
+
+func (f *fakeStep) Run(ctx context.Context, out io.Writer, config steps.Config) error {
+	out.Write([]byte(f.err.Error()))
+	return f.err
+}
+
+func (f *fakeStep) Name() string {
+	return f.name
+}
+
+func (f *fakeStep) Description() string {
+	return f.description
+}
+
 func TestWorkFlowRun(t *testing.T) {
 	workflow := WorkFlow{
 		synchronizer: &fakeSynchronizer{
 			storage: make(map[string]string),
 		},
-		workflowSteps: make([]steps.Step, 4),
+		workflowSteps: []steps.Step{
+			&fakeStep{name: "step1", err: nil},
+			&fakeStep{name: "step2", err: errors.New("shit happens")},
+			&fakeStep{name: "step3", err: nil}},
 	}
 
 	buffer := &bytes.Buffer{}
