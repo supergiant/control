45196615bf74111883b24ce0b075bafd92b16845
stgleb <glebstepanov1992@gmail.com>
2018-08-03 16:05:00 +0300

    Add download k8s binary step

---
+++ pkg/workflows/steps/downloadk8sbinary/download_k8s_binary.go
@@ -0,0 +1,46 @@
+package downloadk8sbinary
+
+import (
+	"context"
+	"io"
+	"text/template"
+
+	"github.com/pkg/errors"
+
+	tm "github.com/supergiant/supergiant/pkg/templatemanager"
+	"github.com/supergiant/supergiant/pkg/workflows/steps"
+)
+
+const StepName = "downloadK8Sbinary"
+
+type Step struct {
+	scriptTemplate *template.Template
+}
+
+func init() {
+	steps.RegisterStep(StepName, New(tm.GetTemplate(StepName)))
+}
+
+func New(tpl *template.Template) *Step {
+	return &Step{
+		scriptTemplate: tpl,
+	}
+}
+
+func (t *Step) Run(ctx context.Context, out io.Writer, config steps.Config) error {
+	err := steps.RunTemplate(context.Background(), t.scriptTemplate,
+		config.Runner, out, nil)
+	if err != nil {
+		return errors.Wrap(err, "download k8s binary step")
+	}
+
+	return nil
+}
+
+func (t *Step) Name() string {
+	return StepName
+}
+
+func (t *Step) Description() string {
+	return ""
+}
