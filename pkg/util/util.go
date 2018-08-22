5a067c194bc9c5733ac5a3844486a75e7eea1b4c
stgleb <glebstepanov1992@gmail.com>
2018-08-22 16:47:28 +0300

    Update name generation utility

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -227,6 +227,6 @@ func RecurseSchema(schema map[string]interface{}, obj interface{}) {
 	}
 }
 
-func MakeNodeName(name, role string) string {
-	return name + "-" + role + "-" + strings.ToLower(RandomString(5))
+func MakeNodeName(role string) string {
+	return role + "-" + strings.ToLower(RandomString(5))
 }
