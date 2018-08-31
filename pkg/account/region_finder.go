771b36edbb60c6cc1cb04fc75ccb543cf4c4f46a
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-08-31 15:53:58 +0300

    Introduced RegionFinder API + digital ocean implementation

---
+++ pkg/account/region_finder.go
@@ -0,0 +1,122 @@
+package account
+
+import (
+	"context"
+
+	"strconv"
+	"sync"
+
+	"github.com/digitalocean/godo"
+	"github.com/pkg/errors"
+	"github.com/supergiant/supergiant/pkg/clouds"
+	"github.com/supergiant/supergiant/pkg/clouds/digitaloceanSDK"
+	"github.com/supergiant/supergiant/pkg/model"
+	"github.com/supergiant/supergiant/pkg/profile"
+)
+
+var (
+	ErrUnsupportedProvider = errors.New("unsupported provider")
+)
+
+//Region represents
+type Region struct {
+	//Human readable name, e.g. New York City 1 or EU West 1 Frankfurt
+	Name string `json:"name"`
+	//API specific ID, e.g. t2.micro
+	ID string `json:"id"`
+
+	//API specific IDs for a node size/type
+	AvailableSizes []string
+}
+
+//RegionSizes represents aggregated information about available regions/azs and node sizes/types
+type RegionSizes struct {
+	Provider clouds.Name         `json:"provider"`
+	Regions  []*Region           `json:"regions"`
+	Sizes    []*profile.NodeSize `json:"sizes"`
+}
+
+//RegionFinder is used to find a list of available regions(availability zones, etc) with available vm types
+//in a given cloud provider using given account credentials
+type RegionFinder interface {
+	//Find returns a slice of cloud specific regions/az's
+	//if not found would return an empty slice
+	Find(context.Context) (*RegionSizes, error)
+}
+
+//GetRegionFinder returns finder attached to corresponding account as it has all credentials for a cloud provider
+func GetRegionFinder(account *model.CloudAccount) (RegionFinder, error) {
+	switch account.Provider {
+	case clouds.DigitalOcean:
+		sdk, err := digitaloceanSDK.NewFromAccount(account)
+		if err != nil {
+			return nil, err
+		}
+		return &digitalOceanRegionFinder{
+			sdk: sdk,
+		}, nil
+	}
+	return nil, ErrUnsupportedProvider
+}
+
+type digitalOceanRegionFinder struct {
+	sdk *digitaloceanSDK.SDK
+}
+
+func (rf *digitalOceanRegionFinder) Find(ctx context.Context) (*RegionSizes, error) {
+	cl := rf.sdk.GetClient()
+	regions := make([]*Region, 0)
+
+	var wg sync.WaitGroup
+	var sizes []godo.Size
+	var sizeErr error
+
+	var doRegions []godo.Region
+	var doErr error
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		sizes, _, sizeErr = cl.Sizes.List(ctx, nil)
+	}()
+	go func() {
+		defer wg.Done()
+		doRegions, _, doErr = cl.Regions.List(ctx, nil)
+	}()
+	//assignment will work fine because of the memory barrier
+	wg.Wait()
+
+	if sizeErr != nil {
+		return nil, sizeErr
+	}
+	if doErr != nil {
+		return nil, doErr
+	}
+
+	nodeSizes := make([]*profile.NodeSize, 0)
+	for _, s := range sizes {
+		ns := &profile.NodeSize{
+			Name: s.Slug,
+			RAM:  strconv.Itoa(s.Memory),
+			CPU:  strconv.Itoa(s.Vcpus),
+		}
+		nodeSizes = append(nodeSizes, ns)
+	}
+
+	for _, r := range doRegions {
+		region := &Region{
+			ID:             r.Slug,
+			Name:           r.Name,
+			AvailableSizes: r.Sizes,
+		}
+		regions = append(regions, region)
+	}
+
+	rs := &RegionSizes{
+		Provider: clouds.DigitalOcean,
+		Regions:  regions,
+		Sizes:    nodeSizes,
+	}
+
+	return rs, nil
+}
---
+++ test/account/finder_test.go
@@ -0,0 +1,81 @@
+package account
+
+import (
+	"os"
+	"testing"
+
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/gorilla/mux"
+	"github.com/pborman/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/supergiant/supergiant/pkg/account"
+	"github.com/supergiant/supergiant/pkg/clouds"
+	"github.com/supergiant/supergiant/pkg/model"
+	"github.com/supergiant/supergiant/pkg/storage"
+	"github.com/supergiant/supergiant/pkg/testutils/assert"
+)
+
+const defaultETCDHost = "http://127.0.0.1:2379"
+
+var defaultConfig clientv3.Config
+
+func init() {
+	assert.MustRunETCD(assert.DefaultETCDURL)
+	defaultConfig = clientv3.Config{
+		Endpoints: []string{defaultETCDHost},
+	}
+}
+
+func TestFindDigitalOceanRegions(t *testing.T) {
+	doToken := os.Getenv(clouds.EnvDigitalOceanAccessToken)
+	require.NotEmpty(t, doToken)
+
+	logrus.SetLevel(logrus.DebugLevel)
+
+	kv := storage.NewETCDRepository(defaultConfig)
+	accounts := account.NewService(account.DefaultStoragePrefix, kv)
+
+	accName := uuid.New()
+	err := accounts.Create(context.Background(), &model.CloudAccount{
+		Name:     accName,
+		Provider: clouds.DigitalOcean,
+		Credentials: map[string]string{
+			clouds.KeyDigitalOceanAccessToken: doToken,
+		},
+	})
+	require.NoError(t, err)
+
+	router := mux.NewRouter()
+	handler := account.NewHandler(accounts)
+	handler.Register(router)
+
+	req := httptest.NewRequest(http.MethodGet, "/accounts/"+accName+"/regions", nil)
+	req = mux.SetURLVars(req, map[string]string{
+		"accountName": accName,
+	})
+	rr := httptest.NewRecorder()
+
+	handler.GetRegions(rr, req)
+	require.NoError(t, err)
+
+	regionsJSON, err := ioutil.ReadAll(rr.Body)
+	require.NoError(t, err)
+	t.Log(string(regionsJSON))
+
+	require.Equal(t, rr.Code, http.StatusOK)
+
+	rs := &account.RegionSizes{}
+	err = json.Unmarshal(regionsJSON, &rs)
+	require.NoError(t, err)
+
+	require.True(t, len(rs.Regions) > 0)
+	require.True(t, len(rs.Sizes) > 0)
+	require.Equal(t, clouds.DigitalOcean, rs.Provider)
+}
