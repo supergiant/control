0afc0762ed272b71b9f86350be7448bb433c2857
nikage <nikage23+1@gmail.com>
2018-09-10 14:50:18 +0300

    minor footer fixes

--- cmd/ui/assets/src/app/app.component.ts
+++ cmd/ui/assets/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router, Event, NavigationEnd } from '@angular/router';
-
-
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,8 +13,7 @@ export class AppComponent {
 
   title = 'app';
   // TODO: generate this dynamically based on img/backgrounds count
-  public bg_count = 12
-  public showHeader = true;
+  public bg_count = 12;
   public location = '';
   public options = {
     position: ['top', 'left'],
@@ -24,17 +21,7 @@ export class AppComponent {
     lastOnBottom: true,
   };
 
-  constructor(private _route: ActivatedRoute, router: Router) {
-    this.location = _route.snapshot.url.join('');
-    router.events.subscribe((e: Event) => { // TODO: think about implementing route guard instead
-      if (e instanceof NavigationEnd) {
-        if (e.urlAfterRedirects === "/" || e.urlAfterRedirects === "/#log-out") {
-          this.showHeader = false;
-        } else {
-          this.showHeader = true;
-        }
-      }
-    })
+  constructor( private router: Router) {
   }
 
   ngAfterViewInit() {
@@ -42,5 +29,9 @@ export class AppComponent {
     // const body = this.body.nativeElement;
     // // TODO: figure out resizing imgs
     // body.style.background = "url(assets/img/backgrounds/bg-" + i.toString() + ".jpg) center top / 1366px 768px no-repeat";
+  }
+
+  get showHeader() {
+    return !Boolean(this.router.url === '/');
   }
 }
--- cmd/ui/assets/src/app/shared/supergiant/auth/auth.service.ts
+++ cmd/ui/assets/src/app/shared/supergiant/auth/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { UtilService } from '../util/util.service';
 
 @Injectable()
