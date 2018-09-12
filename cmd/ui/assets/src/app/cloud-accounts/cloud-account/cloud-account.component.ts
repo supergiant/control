863f8b8898ffe17c269055519669570e0a0a4ae0
nikage <nikage23+1@gmail.com>
2018-09-12 18:35:52 +0300

    update rxjs from ng5 to ng6

--- cmd/ui/assets/src/app/cloud-accounts/cloud-account/cloud-account.component.ts
+++ cmd/ui/assets/src/app/cloud-accounts/cloud-account/cloud-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 import { Supergiant } from '../../shared/supergiant/supergiant.service';
 import { CloudAccountsService } from '../cloud-accounts.service';
 
