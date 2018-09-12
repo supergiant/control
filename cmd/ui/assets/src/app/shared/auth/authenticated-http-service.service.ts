71baf0704fd0a71b8fc8d285db1e83e79edab372
nikage <nikage23+1@gmail.com>
2018-09-12 19:54:57 +0300

    fix lint issues

--- cmd/ui/assets/src/app/shared/auth/authenticated-http-service.service.ts
+++ cmd/ui/assets/src/app/shared/auth/authenticated-http-service.service.ts
@@ -3,6 +3,7 @@ import {throwError as observableThrowError,  Observable } from 'rxjs';
 
 import {catchError} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+// REFACTOR: remove deprecated modules
 import { Request, XHRBackend, RequestOptions, Response, Http, RequestOptionsArgs, Headers } from '@angular/http';
 
 
@@ -16,6 +17,7 @@ export class AuthenticatedHttpService extends Http {
     super(backend, defaultOptions);
   }
 
+  // REFACTOR: remove deprecated method
   request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
     return super.request(url, options).pipe(catchError((error: Response) => {
       if ((error.status === 401 || error.status === 403) && (window.location.href.match(/\?/g) || []).length < 2) {
