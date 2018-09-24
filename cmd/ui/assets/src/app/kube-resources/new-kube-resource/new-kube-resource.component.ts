808b26db017c5c957e30973e83d62eaba5d0c7f4
nikage <42967644+nikage@users.noreply.github.com>
2018-09-24 16:31:53 +0300

    Ng6 update (#660)
    * create basic navigation link styles
    
    * create user menu component
    
    * move footer into separate component
    
    * small fix on user menu circle
    
    * minor footer fixes
    
    * removed extra footer
    
    * fix layout issues
    
    - add router active links
    - fix dashboard page
    
    * fix margin on login page
    
    - remove build warning
    - move h1 to global styles
    
    * fix footer overlay bug
    
    - add sticky footer
    - add sticky background
    
    * fix 404 xhr bundle requests
    
    * update package-lock.json
    
    * update deps
    
    - package-lock.json
    - package.json
    
    * update dependencies for angular 6
    
    * remove unused brace imports
    
    * update rxjs from ng5 to ng6
    
    * add tslint config
    
    - add tslint rules
    
    * fix lint issues
    
    * Add key pair generation for provisioning (#637)
    
    * Add key pair generation for provisioning
    
    * Add test for fingerprint
    
    * Add tests on generate public key
    
    * Add test for generate keypair
    
    * gover get public ip addr with unit test
    
    * cover get private ip addr with unit test
    
    * Extract ssh key creation to separate methods
    
    * small changes on DO step
    
    * update digital ocean
    
    * Add todo
    
    * Use bootstrap keys for entire provisioning process
    
    * Change key creation step for aws
    
    * Remove old config example
    
    * goimports
    
    * cover create key with unit tests
    
    * Review updates
    
    * updates regarding the review
    
    * revert to ng 5
    
    fix on build failure (#646)
    
    Revert "fix lint issues"
    
    This reverts commit 71baf0704fd0a71b8fc8d285db1e83e79edab372.
    
    Revert "add tslint config"
    
    This reverts commit 180f24c0d30874733b851c5ef24b6d8a4e0b6c32.
    
    Revert "update rxjs from ng5 to ng6"
    
    This reverts commit 863f8b8898ffe17c269055519669570e0a0a4ae0.
    
    Revert "remove unused brace imports"
    
    This reverts commit 7acc7fde813a67502d6347dd4810074fe3cc6e61.
    
    Revert "update dependencies for angular 6"
    
    This reverts commit 3d19cb8b95bd441f830c2decf421d91647036d6a.
    
    Revert "update deps"
    
    This reverts commit d9090a8f7f1c63c422866a87c3204a471355dcc3.
    
    Revert "update package-lock.json"
    
    This reverts commit 0bfafb586d78c93244ba407564f88aa5ee6e26ef.
    
    Revert "minor layout fixes (#641)"
    
    This reverts commit 5317aac8e33b4cfee3543b38e2041e667730c11f.
    
    Revert "styles to latest state"
    
    reverted all deps to ng 5
    
    * prepare deps for update
    
    * update angular/core --force
    
    * update ngx-contextmenu
    
    * update angular material
    
    * update rxjs 6
    
    * minor packages updates
    
    * install @angular-devkit/build-angular
    
    * fix deprecated do operator
    
    * install z-schema
    
    * fix ace imports
    
    * fix merge conflicts
    
    * upgrade rxjs imports
    
    * fix merge conflict cloud-accounts.model.ts
    
    * fix ContextMenuModule import

--- cmd/ui/assets/src/app/kube-resources/new-kube-resource/new-kube-resource.component.ts
+++ cmd/ui/assets/src/app/kube-resources/new-kube-resource/new-kube-resource.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 import { Supergiant } from '../../shared/supergiant/supergiant.service';
 import { Notifications } from '../../shared/notifications/notifications.service';
 import { KubeResourcesModel } from '../kube-resources.model';
 
-import "brace/mode/json";
 
 @Component({
   selector: 'app-new-kube-resource',
