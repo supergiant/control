49abd0cd972e07267a7367e1d9c906c34eaa6777
Nick <nikage23+1@gmail.com>
2018-09-28 17:58:37 +0300

    fix app test

--- cmd/ui/assets/src/app/app.component.spec.ts
+++ cmd/ui/assets/src/app/app.component.spec.ts
@@ -2,11 +2,15 @@ import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
     schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -25,10 +29,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('app');
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
 });
