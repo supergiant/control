9f587d46d7687ac15d00a86058d2a6c02a19abb1
Nick <nikage23+1@gmail.com>
2018-10-02 19:56:19 +0300

    create app-list.component

--- cmd/ui/assets/src/app/features/app-store/app-store-routing.module.ts
+++ cmd/ui/assets/src/app/features/app-store/app-store-routing.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppStoreComponent } from './app-store.component';
+import { AppListComponent } from './app-list/app-list.component';
 
-const routes: Routes = [{
-  path: '',
-  component: AppStoreComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    component: AppStoreComponent,
+    children: [
+      {
+        path: '',
+        component: AppListComponent,
+      },
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forChild(routes) ],
+  exports: [ RouterModule ],
 })
-export class AppStoreRoutingModule { }
+export class AppStoreRoutingModule {
+}
--- cmd/ui/assets/src/app/features/app-store/app-store.module.ts
+++ cmd/ui/assets/src/app/features/app-store/app-store.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AppStoreRoutingModule } from './app-store-routing.module';
 import { AppStoreComponent } from './app-store.component';
+import { AppListComponent } from './app-list/app-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AppStoreRoutingModule
   ],
-  declarations: [AppStoreComponent]
+  declarations: [AppStoreComponent, AppListComponent]
 })
 export class AppStoreModule { }
