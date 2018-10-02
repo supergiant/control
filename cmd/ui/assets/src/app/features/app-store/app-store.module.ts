d470a202ce04755fc98bc5ac196eb608ea50c46c
Nick <nikage23+1@gmail.com>
2018-10-02 19:08:33 +0300

    create separate apps module with routing

---
+++ cmd/ui/assets/src/app/features/app-store/app-store.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppStoreRoutingModule } from './app-store-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AppStoreRoutingModule
+  ],
+  declarations: []
+})
+export class AppStoreModule { }
