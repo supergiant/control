cf5bf4ae1b8d3a6ed9c4a784ced637e162750379
Nick <nikage23+1@gmail.com>
2018-10-04 18:53:42 +0300

    create basic layout for chart card on app store

--- cmd/ui/assets/src/app/features/app-store/app-store.module.ts
+++ cmd/ui/assets/src/app/features/app-store/app-store.module.ts
@@ -7,11 +7,13 @@ import { AppsSupergiantComponent } from './apps-supergiant/apps-supergiant.compo
 import { AppsVerifiedComponent } from './apps-verified/apps-verified.component';
 import { AppsOtherComponent } from './apps-other/apps-other.component';
 import { AppsAddComponent } from './apps-add/apps-add.component';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    AppStoreRoutingModule
+    AppStoreRoutingModule,
+    MatCardModule
   ],
   declarations: [AppStoreComponent, AppsSupergiantComponent, AppsVerifiedComponent, AppsOtherComponent, AppsAddComponent]
 })
