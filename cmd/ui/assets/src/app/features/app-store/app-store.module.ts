b09f9da6f88b2d2f1da3f8b9a735feaa3a4ffce5
Nick <nikage23+1@gmail.com>
2018-10-10 22:44:17 +0300

    feature(BreadcrumbsComponent): create breadcrumbs for app details page

--- cmd/ui/assets/src/app/features/app-store/app-store.module.ts
+++ cmd/ui/assets/src/app/features/app-store/app-store.module.ts
@@ -14,6 +14,7 @@ import { EffectsModule }           from '@ngrx/effects';
 import { AppsEffects }             from '../apps/apps.effects';
 import { AppsListComponent }       from './apps-list/apps-list.component';
 import { AppDetailsComponent } from './app-details/app-details.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   imports: [
@@ -30,7 +31,8 @@ import { AppDetailsComponent } from './app-details/app-details.component';
     AppsOtherComponent,
     AppsAddComponent,
     AppsListComponent,
-    AppDetailsComponent
+    AppDetailsComponent,
+    BreadcrumbsComponent
   ]
 })
 export class AppStoreModule {
