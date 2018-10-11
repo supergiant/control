7d27c33e4980a448bf4ef62db453d63ba13a02a8
Nick <nikage23+1@gmail.com>
2018-10-11 16:56:07 +0300

    fix(layout styles) design issues

--- cmd/ui/assets/src/app/features/app-store/app-store.component.ts
+++ cmd/ui/assets/src/app/features/app-store/app-store.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit }    from '@angular/core';
+import { Component } from '@angular/core';
+import { Router }               from "@angular/router";
+import { State }                from "../../reducers";
+import { select, Store }        from "@ngrx/store";
+import { selectAppDetails }     from "../apps/apps.reducer";
+import { Observable }           from "rxjs";
 
 @Component({
   selector: 'app-app-store',
   templateUrl: './app-store.component.html',
-  styleUrls: ['./app-store.component.scss']
+  styleUrls: [ './app-store.component.scss' ]
 })
-export class AppStoreComponent implements OnInit {
+export class AppStoreComponent {
+  showBreadcrumbs: boolean;
+  breadcrumbsData$: Observable<any>;
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    private store: Store<State>
+  ) {
 
-  ngOnInit() {
+    this.router.events.subscribe(() => {
+      const detailPageRegexp = /\/apps\/[a-z]+\/details\/[a-z]/;
+      this.showBreadcrumbs = Boolean(this.router.url.match(detailPageRegexp));
+    });
+
+    this.breadcrumbsData$ = this.store.pipe(
+      select(selectAppDetails)
+    );
   }
-
 }
