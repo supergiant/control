2f2883c94c4b3c6c818155ad4f667b4585c7f5e1
stgleb <glebstepanov1992@gmail.com>
2018-10-26 16:27:51 +0300

    get back cni

---
+++ pkg/workflows/steps/cni/cni.go
@@ -0,0 +1,56 @@
+package cni
+
+import (
+	"context"
+	"io"
+	"text/template"
+
+	"github.com/pkg/errors"
+
+	tm "github.com/supergiant/supergiant/pkg/templatemanager"
+	"github.com/supergiant/supergiant/pkg/workflows/steps"
+)
+
+const StepName = "cni"
+
+type Step struct {
+	script *template.Template
+}
+
+func (s *Step) Rollback(context.Context, io.Writer, *steps.Config) error {
+	return nil
+}
+
+func Init() {
+	steps.RegisterStep(StepName, New(tm.GetTemplate(StepName)))
+}
+
+func New(script *template.Template) *Step {
+	t := &Step{
+		script: script,
+	}
+
+	return t
+}
+
+func (s *Step) Run(ctx context.Context, out io.Writer, config *steps.Config) error {
+	err := steps.RunTemplate(ctx, s.script, config.Runner, out, nil)
+
+	if err != nil {
+		return errors.Wrap(err, "install cni step")
+	}
+
+	return nil
+}
+
+func (s *Step) Name() string {
+	return StepName
+}
+
+func (s *Step) Description() string {
+	return ""
+}
+
+func (s *Step) Depends() []string {
+	return nil
+}
