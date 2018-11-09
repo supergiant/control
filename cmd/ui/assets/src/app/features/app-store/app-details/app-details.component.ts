534e45acb5873a5842f85009cd6aa18f79c96a5e
nikage <42967644+nikage@users.noreply.github.com>
2018-11-09 18:18:25 +0200

    configure fix (#802)
    * configure fix
    
    * configure yml fix

--- cmd/ui/assets/src/app/features/app-store/app-details/app-details.component.ts
+++ cmd/ui/assets/src/app/features/app-store/app-details/app-details.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit }             from '@angular/core';
+import { Component, OnInit, OnDestroy }  from '@angular/core';
 import { ActivatedRoute }                from "@angular/router";
 import { LoadAppDetails, SetAppDetails } from "../../apps/actions";
 import { State }                         from "../../../reducers";
 import { select, Store }                 from "@ngrx/store";
 import { Chart, selectAppDetails }       from "../../apps/apps.reducer";
-import { Observable }                    from "rxjs";
+import { Observable, Subscription }      from "rxjs";
 import { MatDialog }                     from "@angular/material";
 import { DeployComponent }               from "./deploy/deploy.component";
 import { ConfigureComponent }            from "./confure/configure.component";
-import { map, switchMap, tap, distinct } from "rxjs/operators";
+import { map, switchMap, tap, filter }   from "rxjs/operators";
 
 @Component({
   selector: 'app-details',
   templateUrl: './app-details.component.html',
   styleUrls: ['./app-details.component.scss']
 })
-export class AppDetailsComponent implements OnInit {
+export class AppDetailsComponent implements OnInit, OnDestroy {
   chartDetails$: Observable<Chart>;
+  private subscriptons: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private store: Store<State>,
     public dialog: MatDialog,
   ) {
+    this.subscriptons = new Subscription();
 
     let repo = this.route.snapshot.paramMap.get('repo');
     let chart = this.route.snapshot.paramMap.get('chart');
@@ -45,14 +47,14 @@ export class AppDetailsComponent implements OnInit {
   }
 
   openConfigureDialog() {
-    this.dialog.open(ConfigureComponent, {
+    const dialogSub = this.dialog.open(ConfigureComponent, {
       width: `1024px`,
       data: {
         chart$: this.chartDetails$,
       }
     }).afterClosed()
       .pipe(
-        distinct(),
+        filter(values => values !== undefined),
         map(
           values => this.chartDetails$.pipe(
             map(chart => {
@@ -66,8 +68,13 @@ export class AppDetailsComponent implements OnInit {
           this.store.dispatch(new SetAppDetails(updatedValues));
         })
       ).subscribe(val => {
-      console.log('res1', val);
-    });
+        console.log('res1', val);
+      });
 
+    this.subscriptons.add(dialogSub);
+  }
+
+  ngOnDestroy() {
+    this.subscriptons.unsubscribe();
   }
 }
