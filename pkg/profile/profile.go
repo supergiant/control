fef2df040996ea912bc259fff6f044cfa3fdae92
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-11-09 17:42:28 +0200

    Fixed issue with AWS ssh keys.

--- pkg/profile/profile.go
+++ pkg/profile/profile.go
@@ -10,19 +10,21 @@ type Profile struct {
 
 	// TODO(stgleb): In future releases arch will probably migrate to node profile
 	// to allow user create heterogeneous cluster of machine with different arch
-	Provider              clouds.Name           `json:"provider" valid:"in(aws|digitalocean|packet|gce|openstack)" valid:"-"`
-	Region                string                `json:"region" valid:"-"`
-	Arch                  string                `json:"arch" valid:"-"`
-	OperatingSystem       string                `json:"operatingSystem" valid:"-"`
-	UbuntuVersion         string                `json:"ubuntuVersion" valid:"-"`
-	DockerVersion         string                `json:"dockerVersion" valid:"-"`
-	K8SVersion            string                `json:"K8SVersion" valid:"-"`
-	FlannelVersion        string                `json:"flannelVersion" valid:"-"`
-	NetworkType           string                `json:"networkType" valid:"-"`
-	CIDR                  string                `json:"cidr" valid:"-"`
-	HelmVersion           string                `json:"helmVersion" valid:"-"`
-	RBACEnabled           bool                  `json:"rbacEnabled" valid:"-"`
-	CloudSpecificSettings CloudSpecificSettings `json:"cloudSpecificSettings" valid:"-"`
+	Provider               clouds.Name           `json:"provider" valid:"in(aws|digitalocean|packet|gce|openstack)" valid:"-"`
+	Region                 string                `json:"region" valid:"-"`
+	Arch                   string                `json:"arch" valid:"-"`
+	OperatingSystem        string                `json:"operatingSystem" valid:"-"`
+	UbuntuVersion          string                `json:"ubuntuVersion" valid:"-"`
+	DockerVersion          string                `json:"dockerVersion" valid:"-"`
+	K8SVersion             string                `json:"K8SVersion" valid:"-"`
+	FlannelVersion         string                `json:"flannelVersion" valid:"-"`
+	NetworkType            string                `json:"networkType" valid:"-"`
+	CIDR                   string                `json:"cidr" valid:"-"`
+	HelmVersion            string                `json:"helmVersion" valid:"-"`
+	RBACEnabled            bool                  `json:"rbacEnabled" valid:"-"`
+	CloudSpecificSettings  CloudSpecificSettings `json:"cloudSpecificSettings" valid:"-"`
+	PublicKey              string                `json:"publicKey" valid:"-"`
+	LogBootstrapPrivateKey bool                  `json:"logBootstrapPrivateKey" valid:"-"`
 }
 
 type NodeProfile map[string]string
