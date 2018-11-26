806b17232d283165eb29664a1a9a8795beea969b
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-11-26 20:39:50 +0200

    fixed building twice.

--- push_release.sh
+++ push_release.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 echo "building binary..."
-make build
-
 # if a tag has alpha or beta in the name, it will be released as a pre-release.
 # if a tag does not have alpha or beta, it is pushed as a full release.
 case "${TAG}" in
 	*alpha* )  echo "Releasing version: ${TAG}, as pre-release"
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG" /go/bin/supergiant;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG" ${GOPATH}/bin/supergiant;;
 	*beta* )    echo "Releasing version: ${TAG}, as pre-release"
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG"  /go/bin/supergiant;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG"  ${GOPATH}/bin/supergiant;;
 	*)echo "Releasing version: ${TAG}, as latest release."
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "latest release" --debug "$TAG"  /go/bin/supergiant;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "latest release" --debug "$TAG"  ${GOPATH}/bin/supergiant;;
 esac
 
 # Check for errors
