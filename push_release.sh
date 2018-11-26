118db697773e1d277e7a81ba2afed2dafa6253df
Yegor Chumakov <yegor.chumakov@gmail.com>
2018-11-26 18:28:30 +0200

    fixed push release script

--- push_release.sh
+++ push_release.sh
@@ -7,11 +7,11 @@ make build
 # if a tag does not have alpha or beta, it is pushed as a full release.
 case "${TAG}" in
 	*alpha* )  echo "Releasing version: ${TAG}, as pre-release"
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG" ${GOPATH}/bin/controlplane;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG" /go/bin/supergiant;;
 	*beta* )    echo "Releasing version: ${TAG}, as pre-release"
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG" ${GOPATH}/bin/controlplane;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG"  /go/bin/supergiant;;
 	*)echo "Releasing version: ${TAG}, as latest release."
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "latest release" --debug "$TAG" ${GOPATH}/bin/controlplane;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "latest release" --debug "$TAG"  /go/bin/supergiant;;
 esac
 
 # Check for errors
