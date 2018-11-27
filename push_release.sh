7833e76b1f78a60e9410d193280906b8090ebf52
Eldridge Henley <35240784+eldridgeh@users.noreply.github.com>
2018-11-27 13:09:19 -0600

    Adding osx and windows build for control plane (#1019)

--- push_release.sh
+++ push_release.sh
@@ -5,24 +5,24 @@ PROJECTDIR=${TRAVIS_HOME}/gopath/src/github.com/${TRAVIS_REPO_SLUG}
 echo ${PROJECTDIR}
 
 mkdir -p ./supergiant-build
-cp ./controlplane ./supergiant-build
+
 cp -avr ${PROJECTDIR}/templates/ ./supergiant-build
 cp -avr ${PROJECTDIR}/cmd/ui/assets/dist/ ./supergiant-build
 chmod +x ./supergiant-build/controlplane
 
 ls -la ./supergiant-build
 
-tar -czpf /tmp/assest.gz ./supergiant-build
+tar -czpf dist/assest.gz ./supergiant-build
 
 # if a tag has alpha or beta in the name, it will be released as a pre-release.
 # if a tag does not have alpha or beta, it is pushed as a full release.
 case "${TAG}" in
 	*alpha* )  echo "Releasing version: ${TAG}, as pre-release"
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG"  /tmp/assest.gz;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG"  dist/;;
 	*beta* )    echo "Releasing version: ${TAG}, as pre-release"
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG"   /tmp/assest.gz;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "pre-release" --prerelease --debug "$TAG"   dist/;;
 	*)echo "Releasing version: ${TAG}, as latest release."
-	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "latest release" --debug "$TAG"   /tmp/assest.gz;;
+	ghr --username supergiant --token "$GITHUB_TOKEN" --replace -b "latest release" --debug "$TAG"   dist/;;
 esac
 
 # Check for errors
