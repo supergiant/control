002c02756ac464b8445d3db4decb77f840a9f7d9
Gleb <glebstepanov1992@gmail.com>
2018-11-30 22:14:05 +0200

    Enable authorization through websocket (#1082)
    * Enable authorization through websocket
    
    * goimports
    
    * gofmt

--- pkg/workflows/handler.go
+++ pkg/workflows/handler.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -209,6 +210,14 @@ func (h *TaskHandler) BuildAndRunTask(w http.ResponseWriter, r *http.Request) {
 
 // NOTE(stgleb): This is made for testing purposes and example, remove when UI is done.
 func (h *TaskHandler) GetLogs(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	tokenString := ""
+	ts := strings.Split(authHeader, " ")
+
+	if len(ts) > 1 {
+		tokenString = ts[1]
+	}
+
 	page := `<!DOCTYPE html>
 <html lang="en">
     <head>
@@ -220,7 +229,7 @@ func (h *TaskHandler) GetLogs(w http.ResponseWriter, r *http.Request) {
         <div id="logs"></div>
         <script type="text/javascript">
             (function() {
-                var conn = new WebSocket("ws://{{ .Host }}/tasks/{{ .TaskID }}/logs");
+                var conn = new WebSocket("ws://{{ .Host }}/v1/api/tasks/{{ .TaskID }}/logs", "{{ .Token }}");
                 conn.onmessage = function(evt) {
                     console.log('file updated');
  					$('#logs').append("<p>" + evt.data + "</p>");
@@ -242,9 +251,11 @@ func (h *TaskHandler) GetLogs(w http.ResponseWriter, r *http.Request) {
 	var v = struct {
 		Host   string
 		TaskID string
+		Token  string
 	}{
 		r.Host,
 		id,
+		tokenString,
 	}
 	err := tpl.Execute(w, &v)
 	if err != nil {
