ac1724e13a586e1ff44795d8272491bf4fd5f6c8
stgleb <glebstepanov1992@gmail.com>
2018-12-02 01:20:50 +0200

    Fix task restart when task has been failed on create instance step and didn't obtain public IP

--- pkg/workflows/utils.go
+++ pkg/workflows/utils.go
@@ -3,8 +3,8 @@ package workflows
 import (
 	"encoding/json"
 
-	"github.com/supergiant/control/pkg/runner/ssh"
 	"github.com/supergiant/control/pkg/storage"
+	"github.com/supergiant/control/pkg/runner/ssh"
 )
 
 func DeserializeTask(data []byte, repository storage.Interface) (*Task, error) {
@@ -19,18 +19,24 @@ func DeserializeTask(data []byte, repository storage.Interface) (*Task, error) {
 	task.repository = repository
 	task.workflow = GetWorkflow(task.Type)
 
-	cfg := ssh.Config{
-		Host:    task.Config.Node.PublicIp,
-		Port:    task.Config.SshConfig.Port,
-		User:    task.Config.SshConfig.User,
-		Timeout: task.Config.SshConfig.Timeout,
-		Key:     []byte(task.Config.SshConfig.BootstrapPrivateKey),
-	}
 
-	task.Config.Runner, err = ssh.NewRunner(cfg)
+	// NOTE(stgleb): If step has failed on machine creation state
+	// public ip will be blank and lead to error when restart
+	// TODO(stgleb): Move ssh runner creation to task Restart method
+	if task.Config.Node.PublicIp != "" {
+		cfg := ssh.Config{
+			Host:    task.Config.Node.PublicIp,
+			Port:    task.Config.SshConfig.Port,
+			User:    task.Config.SshConfig.User,
+			Timeout: task.Config.SshConfig.Timeout,
+			Key:     []byte(task.Config.SshConfig.BootstrapPrivateKey),
+		}
 
-	if err != nil {
-		return nil, err
+		task.Config.Runner, err = ssh.NewRunner(cfg)
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return task, nil
