4f1ef561b27ded7f03ffa1c7ef5cae3bfa0f8d2d
nikage <42967644+nikage@users.noreply.github.com>
2018-12-06 16:50:22 +0200

    add default values for nodes config (#1155)

--- cmd/ui/assets/src/app/clusters/cluster/task-logs/task-logs.component.ts
+++ cmd/ui/assets/src/app/clusters/cluster/task-logs/task-logs.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, Inject, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  Inject,
+  ViewChild,
+  AfterContentInit
+} from '@angular/core';
 
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 import { Supergiant } from '../../../shared/supergiant/supergiant.service';
 
@@ -9,37 +15,35 @@ import { Supergiant } from '../../../shared/supergiant/supergiant.service';
   templateUrl: './task-logs.component.html',
   styleUrls: ['./task-logs.component.scss']
 })
-export class TaskLogsComponent implements OnInit, OnDestroy, AfterViewInit {
+export class TaskLogsComponent implements OnDestroy, AfterContentInit {
 
   constructor(
     public dialogRef: MatDialogRef<TaskLogsComponent>,
     private supergiant: Supergiant,
     @Inject(MAT_DIALOG_DATA) public data: any
-
-  ) { this.taskId = data.taskId }
+  ) {
+    this.taskId = data.taskId
+  }
 
   @ViewChild("editor") editor;
 
-  conn: any
+  conn: any;
 
   taskId: any;
   logsString = "";
-  logsArray = [];
-  userScrolled = false
-
-  userScrollEvent(e) { console.log(e) }
+  userScrolled = false;
 
   scrollToBottom() {
     // console.log(this.editor.getEditor());
-    const renderer = this.editor.getEditor().renderer
+    const renderer = this.editor.getEditor().renderer;
     const height = renderer.$size.scrollerHeight;
     if (renderer.scrollTop > 700) {
-      this.editor.getEditor().renderer.scrollTop = this.editor.getEditor().renderer.scrollTop + ( height * 2 );
+      this.editor.getEditor().renderer.scrollTop = this.editor.getEditor().renderer.scrollTop + (height * 2);
     }
   }
 
   updateLogs(e) {
-    const newMessage = e.data + "\r\n"
+    const newMessage = e.data + "\r\n";
     this.logsString = this.logsString + newMessage;
     if (!this.userScrolled) {
       this.scrollToBottom();
@@ -51,14 +55,13 @@ export class TaskLogsComponent implements OnInit, OnDestroy, AfterViewInit {
     const hostname = this.data.hostname;
 
     this.conn = new WebSocket("ws://" + hostname + ":8080/v1/api/tasks/" + taskId + "/logs?token=" + token);
-    this.conn.onmessage = e => { setTimeout(() => this.updateLogs(e), 1); }
+    this.conn.onmessage = e => {
+      setTimeout(() => this.updateLogs(e), 1);
+    }
   }
 
-  ngOnInit() {
+  ngAfterContentInit() {
     this.openConn(this.taskId);
-  }
-
-  ngAfterViewInit() {
   }
 
   ngOnDestroy() {
