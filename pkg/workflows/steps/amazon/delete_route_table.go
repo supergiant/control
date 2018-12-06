7b9936259cc106fff0c1fc364f6420675c58f320
Serhii Aheienko <serhii.aheienko@gmail.com>
2018-12-06 16:47:48 +0200

    Add step for finding amazon machine image (#1154)

--- pkg/workflows/steps/amazon/delete_route_table.go
+++ pkg/workflows/steps/amazon/delete_route_table.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"io"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+
 	"github.com/supergiant/control/pkg/workflows/steps"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/aws"
 )
 
 const DeleteRouteTableStepName = "aws_delete_route_table"
