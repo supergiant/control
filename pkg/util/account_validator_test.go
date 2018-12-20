a0596b5a892026f11caf79e620e115b3374eb262
Gleb <glebstepanov1992@gmail.com>
2018-12-20 17:20:37 +0200

    cover util package with tests (#1221)
    * cover util package with tests
    
    * remove sensitive data from test

--- pkg/util/account_validator_test.go
+++ pkg/util/account_validator_test.go
@@ -10,12 +10,13 @@ import (
 
 func TestValidateCredentials(t *testing.T) {
 	testCases := []struct {
+		description   string
 		cloudAccount  *model.CloudAccount
-		digitalOcean  func(map[string]string) error
-		aws           func(map[string]string) error
+		getCreds      func(map[string]string) error
 		expectedError error
 	}{
 		{
+			description: "unsupported provider",
 			cloudAccount: &model.CloudAccount{
 				Name:        "test",
 				Provider:    "unknown",
@@ -24,45 +25,73 @@ func TestValidateCredentials(t *testing.T) {
 			expectedError: sgerrors.ErrUnsupportedProvider,
 		},
 		{
+			description: "digitalocean",
 			cloudAccount: &model.CloudAccount{
 				Name:        "test",
 				Provider:    clouds.DigitalOcean,
 				Credentials: map[string]string{},
 			},
-			digitalOcean: func(map[string]string) error {
+			getCreds: func(map[string]string) error {
 				return nil
 			},
 			expectedError: nil,
 		},
 		{
+			description: "digitalOCean wrong creds",
 			cloudAccount: &model.CloudAccount{
 				Name:        "test",
 				Provider:    clouds.DigitalOcean,
 				Credentials: map[string]string{},
 			},
-			digitalOcean: func(map[string]string) error {
+			getCreds: func(map[string]string) error {
 				return sgerrors.ErrInvalidCredentials
 			},
 			expectedError: sgerrors.ErrInvalidCredentials,
 		},
 		{
+			description: "aws",
 			cloudAccount: &model.CloudAccount{
 				Name:        "test",
 				Provider:    clouds.AWS,
 				Credentials: map[string]string{},
 			},
-			aws: func(map[string]string) error {
+			getCreds: func(map[string]string) error {
 				return nil
 			},
 			expectedError: nil,
 		},
 		{
+			description: "gce",
+			cloudAccount: &model.CloudAccount{
+				Name:        "test",
+				Provider:    clouds.GCE,
+				Credentials: map[string]string{},
+			},
+			getCreds: func(map[string]string) error {
+				return nil
+			},
+			expectedError: nil,
+		},
+		{
+			description: "gce invalid creds",
+			cloudAccount: &model.CloudAccount{
+				Name:        "test",
+				Provider:    clouds.GCE,
+				Credentials: map[string]string{},
+			},
+			getCreds: func(map[string]string) error {
+				return sgerrors.ErrInvalidCredentials
+			},
+			expectedError: sgerrors.ErrInvalidCredentials,
+		},
+		{
+			description: "aws invalid creads",
 			cloudAccount: &model.CloudAccount{
 				Name:        "test",
 				Provider:    clouds.AWS,
 				Credentials: map[string]string{},
 			},
-			aws: func(map[string]string) error {
+			getCreds: func(map[string]string) error {
 				return sgerrors.ErrInvalidCredentials
 			},
 			expectedError: sgerrors.ErrInvalidCredentials,
@@ -70,9 +99,11 @@ func TestValidateCredentials(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
+		t.Log(testCase.description)
 		validator := CloudAccountValidatorImpl{
-			digitalOcean: testCase.digitalOcean,
-			aws:          testCase.aws,
+			digitalOcean: testCase.getCreds,
+			aws:          testCase.getCreds,
+			gce:          testCase.getCreds,
 		}
 
 		err := validator.ValidateCredentials(testCase.cloudAccount)
@@ -80,5 +111,25 @@ func TestValidateCredentials(t *testing.T) {
 		if err != testCase.expectedError {
 			t.Errorf("Expected error %v actual %v", testCase.expectedError, err)
 		}
+	}
+}
+
+func TestNewCloudAccountValidator(t *testing.T) {
+	validator := NewCloudAccountValidator()
+
+	if validator == nil {
+		t.Errorf("validator must not be nil")
+	}
+
+	if validator.aws == nil {
+		t.Errorf("aws must not be nil")
+	}
+
+	if validator.gce == nil {
+		t.Errorf("gce must not be nil")
+	}
+
+	if validator.digitalOcean == nil {
+		t.Errorf("digitalocean must not be nil")
 	}
 }
