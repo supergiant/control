951d0e19f8496ae89609dfe17ca1380b480a13ee
Gleb <glebstepanov1992@gmail.com>
2019-01-02 12:18:42 +0200

    Cover PKI package with tests (#1231)

--- pkg/pki/helpers.go
+++ pkg/pki/helpers.go
@@ -3,8 +3,6 @@ package pki
 import (
 	"crypto/rsa"
 	"crypto/x509"
-	"fmt"
-
 	"github.com/pkg/errors"
 	certutil "k8s.io/client-go/util/cert"
 )
@@ -12,6 +10,7 @@ import (
 // newCertificateAuthority creates new certificate and private key for the certificate authority
 func newCertificateAuthority() (*x509.Certificate, *rsa.PrivateKey, error) {
 	key, err := certutil.NewPrivateKey()
+
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "create private key")
 	}
@@ -22,21 +21,6 @@ func newCertificateAuthority() (*x509.Certificate, *rsa.PrivateKey, error) {
 	cert, err := certutil.NewSelfSignedCACert(config, key)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "create self-signed certificate")
-	}
-
-	return cert, key, nil
-}
-
-// NewCertAndKey creates signed certificate and key for the provided CA.
-func NewCertAndKey(caCert *x509.Certificate, caKey *rsa.PrivateKey, config *certutil.Config) (*x509.Certificate, *rsa.PrivateKey, error) {
-	key, err := certutil.NewPrivateKey()
-	if err != nil {
-		return nil, nil, fmt.Errorf("create private key: %s", err)
-	}
-
-	cert, err := certutil.NewSignedCert(*config, key, caCert, caKey)
-	if err != nil {
-		return nil, nil, fmt.Errorf("create signed cert: %s", err)
 	}
 
 	return cert, key, nil
