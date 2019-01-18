4b4afeb74fea81f1f2251398600c3c018a8ba4fc
josephkane <josephckane@gmail.com>
2019-01-18 11:24:24 -0600

    Coldstart UI
    * prompt user to set login and password when coldstart
    
    * auto-login on valid signup
    
    * password is min 8 char, upper and lowercase, and must contain at least 1 number
    
    * login is required, but otherwise has no validation

--- cmd/ui/assets/src/app/login/login.component.ts
+++ cmd/ui/assets/src/app/login/login.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Supergiant } from '../shared/supergiant/supergiant.service';
 import { Router } from '@angular/router';
-import { Observable ,  Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { Observable, Subscription, of } from 'rxjs';
+import { catchError, switchMap, zip } from 'rxjs/operators';
+
+import { Supergiant } from '../shared/supergiant/supergiant.service';
+import { UtilService } from '../shared/supergiant/util/util.service';
 import { Notifications } from '../shared/notifications/notifications.service';
 
 @Component({
@@ -10,17 +14,28 @@ import { Notifications } from '../shared/notifications/notifications.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnDestroy {
-  public login: string;
-  public password: string;
+export class LoginComponent implements OnDestroy, OnInit {
   private subscriptions = new Subscription();
-  public status: string;
+  public isColdStart: boolean;
+  public loginForm: FormGroup;
 
   constructor(
     private supergiant: Supergiant,
     private router: Router,
     private notifications: Notifications,
+    private util: UtilService,
+    private formBuilder: FormBuilder
   ) { }
+
+  initColdStartForm() {
+    const pattern = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]')
+    this.loginForm.controls['password'].setValidators([
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(pattern)
+    ]);
+    this.loginForm.controls['password'].updateValueAndValidity()
+  }
 
   error(msg) {
     this.notifications.display(
@@ -29,26 +44,57 @@ export class LoginComponent implements OnDestroy {
       'Error:' + msg);
   }
 
-  onSubmit() {
-    this.status = 'status status-transitioning';
-    const creds = { 'login': this.login, 'password': this.password };
-
+  login(creds) {
     this.supergiant.Auth.login(creds).subscribe(
       res => {
-        if (res['status'] === 200) {
-          this.supergiant.loginSuccess = true;
-          this.router.navigate(['/dashboard']);
-        }
+        this.supergiant.loginSuccess = true;
+        this.router.navigate(['/dashboard']);
       },
       err => {
-        this.status = 'status status-danger';
-        this.error('Invalid Login');
+        this.error(err.userMessage)
       }
-    );
+    )
+  }
+
+  onSubmit() {
+    const creds = this.loginForm.value
+
+    if (this.isColdStart) {
+      this.supergiant.Auth.signup(creds).pipe(
+        switchMap(res => this.supergiant.Auth.login(creds)),
+        catchError((err) => of(err))
+      ).subscribe(
+        res => {
+          this.supergiant.loginSuccess = true;
+          this.router.navigate(['/dashboard']);
+        },
+        err => this.error(err.userMessage)
+      )
+    } else {
+      this.login(creds)
+    }
   }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+
+    this.util.fetch('/coldstart').subscribe(
+      res => {
+        this.isColdStart = res.isColdStart;
+
+        if (this.isColdStart) {
+          this.initColdStartForm();
+        }
+      },
+      err => this.error(err.userMessage)
+    )
   }
 
 }
