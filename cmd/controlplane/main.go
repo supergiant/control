ab613ac9f1b13e75cef77f8c42e2dc34670cec3a
Gleb <glebstepanov1992@gmail.com>
2019-01-30 13:18:35 +0200

    Add file and memory storage (#1271)
    * Add file and memory storage
    
    * use bbolt
    
    * update deps
    
    * finish different storage types
    
    * remove binary
    
    * rename storageType to storageMode
    
    * Change camel case to dash separated
    
    * Add more tests and change command line arg style

--- cmd/controlplane/main.go
+++ cmd/controlplane/main.go
@@ -18,7 +18,8 @@ var (
 	version       = "undefined"
 	addr          = flag.String("address", "0.0.0.0", "network interface to attach server to")
 	port          = flag.Int("port", 8080, "tcp port to listen for incoming requests")
-	etcdURL       = flag.String("etcd-url", "localhost:2379", "etcd url with port")
+	storageMode   = flag.String("storage-mode", "file", "storage type either file(default), memory or etcd")
+	storageURI    = flag.String("storage-uri", "supertiant.db", "uri of storage depends on selected storage type, for memory storage type this is empty")
 	templatesDir  = flag.String("templates", "/etc/supergiant/templates/", "supergiant will load script templates from the specified directory on start")
 	logLevel      = flag.String("log-level", "INFO", "logging level, e.g. info, warning, debug, error, fatal")
 	logFormat     = flag.String("log-format", "txt", "logging format [txt json]")
@@ -39,7 +40,8 @@ func main() {
 	cfg := &controlplane.Config{
 		Addr:          *addr,
 		Port:          *port,
-		EtcdUrl:       *etcdURL,
+		StorageMode:   *storageMode,
+		StorageURI:    *storageURI,
 		TemplatesDir:  *templatesDir,
 		ReadTimeout:   time.Second * 20,
 		WriteTimeout:  time.Second * 10,
---
+++ pkg/storage/file/file.go
@@ -0,0 +1,111 @@
+package file
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+
+	"github.com/etcd-io/bbolt"
+	"github.com/supergiant/control/pkg/sgerrors"
+)
+
+const bucketName = "supergiant.io"
+
+type FileRepository struct {
+	db *bbolt.DB
+}
+
+func NewFileRepository(fileName string) (*FileRepository, error) {
+	db, err := bbolt.Open(fileName, 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileRepository{
+		db: db,
+	}, nil
+}
+
+func (i *FileRepository) Get(ctx context.Context, prefix string, key string) ([]byte, error) {
+	var value []byte
+
+	err := i.db.View(func(tx *bbolt.Tx) error {
+		value = tx.Bucket([]byte(bucketName)).Get([]byte(prefix + key))
+
+		if value == nil {
+			return sgerrors.ErrNotFound
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
+func (i *FileRepository) Put(ctx context.Context, prefix string, key string, value []byte) error {
+	err := i.db.Update(func(tx *bbolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+
+		err = bucket.Put([]byte(prefix+key), value)
+		return err
+	})
+
+	return err
+}
+
+func (i *FileRepository) Delete(ctx context.Context, prefix string, key string) error {
+	err := i.db.Update(func(tx *bbolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+
+		return bucket.Delete([]byte(prefix + key))
+	})
+
+	return err
+}
+
+func (i *FileRepository) GetAll(ctx context.Context, prefix string) ([][]byte, error) {
+	values := make([][]byte, 0)
+
+	err := i.db.View(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		cursor := bucket.Cursor()
+		prefixBytes := []byte(prefix)
+
+		for k, v := cursor.Seek(prefixBytes); k != nil && bytes.HasPrefix(k, prefixBytes); k, v = cursor.Next() {
+			values = append(values, v)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return values, nil
+}
