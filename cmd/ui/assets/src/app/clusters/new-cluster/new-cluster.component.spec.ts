2d117667af98c7beadf32aa091d6994ab4c2dc79
nikage <42967644+nikage@users.noreply.github.com>
2019-02-06 16:07:41 +0200

    Master size selection should generate feedback (#1282)
    * Master size selection feedback for all cloud accounts
    
    - aws
    - gce
    - digital ocean
    
    * add real-time recalculation for cluster size feedback
    
    * fix (new-cluster.component) blocking error on machine config step
    
    * fix (new-cluster.component) unit test error
    
    * refactor (new-cluster.component) minor changes
    
    - code-style
    - naming
    
    * refactor (new-cluster.component) hotfix and unit tests
    
    * refactor (new-cluster.component) fix design issue on master size recommendation

--- cmd/ui/assets/src/app/clusters/new-cluster/new-cluster.component.spec.ts
+++ cmd/ui/assets/src/app/clusters/new-cluster/new-cluster.component.spec.ts
@@ -1,7 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
+import { MatHorizontalStepper } from '@angular/material';
 
-import { NewClusterComponent } from './new-cluster.component';
+import { NewClusterComponent, StepIndexes } from './new-cluster.component';
 import { DEFAULT_MACHINE_SET } from 'app/clusters/new-cluster/new-cluster.component.config';
 import { CLUSTER_OPTIONS } from 'app/clusters/new-cluster/cluster-options.config';
 import { NEW_CLUSTER_MODULE_METADATA } from './new-cluster.component.metadata';
@@ -25,8 +26,15 @@ describe('NewClusterComponent', () => {
     component = fixture.componentInstance;
     getClustersSpy = spyOn(component, 'getClusters').and.callThrough();
     fixture.detectChanges();
-
   });
+
+  function clickBtnNext() {
+    const debugEl: DebugElement = fixture.debugElement;
+    debugEl
+      .nativeElement
+      .querySelector('button[matStepperNext]')
+      .click();
+  }
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -48,14 +56,31 @@ describe('NewClusterComponent', () => {
     expect(component.provisioning).toEqual(false);
   });
 
+  function fillAccountDetails() {
+    const controls = component.nameAndCloudAccountForm.controls;
+    controls['name'].setValue('FOO');
+    controls['cloudAccount'].setValue('BAR');
+  }
+
+  function goToStep2() {
+    fillAccountDetails();
+    clickBtnNext();
+  }
+
+  function goToStep3() {
+    goToStep2();
+    clickBtnNext();
+  }
+
   describe('STEPS', () => {
-    let stepper;
+    let stepper: MatHorizontalStepper;
+
+    beforeEach(() => {
+      stepper = fixture.componentInstance.stepper;
+    });
+
 
     describe('STEP 1: Name and Cloud account', () => {
-      beforeEach(() => {
-        stepper = fixture.componentInstance.stepper;
-      });
-
       it('should NOT proceed to the next step until the form is filled', () => {
         const stepperSpy = spyOn(stepper, 'next').and.callThrough();
         component.nextStep();
@@ -64,21 +89,39 @@ describe('NewClusterComponent', () => {
       });
 
       it('should PROCEED to the next step until the form is filled', () => {
-        const debugEl: DebugElement = fixture.debugElement;
         const nextStepSpy = spyOn(stepper, 'next').and.callThrough();
-        const controls = component.nameAndCloudAccountForm.controls;
-
-        controls['name'].setValue('FOO');
-        controls['cloudAccount'].setValue('BAR');
-
-        debugEl
-          .nativeElement
-          .querySelector('button[matStepperNext]')
-          .click();
+        goToStep2();
 
         fixture.detectChanges();
 
         expect(nextStepSpy).toHaveBeenCalled();
+      });
+    });
+
+    describe('STEP 2: Cluster Configuration', () => {
+      beforeEach(() => {
+        goToStep2();
+      });
+
+      it('should be on the Cluster Config tab', () => {
+        expect(stepper.selectedIndex).toEqual(StepIndexes.ClusterConfig);
+      });
+
+      //  TODO: check all the fields set to default values
+    });
+
+    describe('STEP 3: Cluster Configuration', () => {
+      beforeEach(() => {
+        goToStep3();
+      });
+
+      it('should be on the Provider config tab', () => {
+        expect(stepper.selectedIndex).toEqual(StepIndexes.ProvideConfig);
+      });
+
+      // TODO
+      xit('should NOT PROCEED until machine config is valid', () => {
+        expect(stepper.selectedIndex).toEqual(StepIndexes.ProvideConfig);
       });
     });
   });
