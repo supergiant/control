dd5a7c4df18786ed5f8d24996c866000ac1ffa93
stgleb <glebstepanov1992@gmail.com>
2019-03-07 14:33:06 +0200

    make build-ui once

--- docker_build.sh
+++ docker_build.sh
@@ -3,7 +3,6 @@
 echo "$TRAVIS_REPO_SLUG":"$TAG"
 # build the docker container
 echo "Building Docker container"
-make build-ui
 
 docker build --tag "$TRAVIS_REPO_SLUG":"$TAG" .
 if [[ "$TRAVIS_TAG" =~ ^v[0-9]. ]]; then
