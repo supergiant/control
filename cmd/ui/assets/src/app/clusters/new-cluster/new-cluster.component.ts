bb91ef1c8fc6d642483a9e46e0de3d7daeeca32c
stgleb <glebstepanov1992@gmail.com>
2019-03-08 21:46:14 +0200

    K8S version bump

--- cmd/ui/assets/src/app/clusters/new-cluster/new-cluster.component.ts
+++ cmd/ui/assets/src/app/clusters/new-cluster/new-cluster.component.ts
@@ -102,7 +102,7 @@ export class NewClusterComponent implements OnInit, OnDestroy {
     });
 
     this.clusterConfig = this.formBuilder.group({
-      K8sVersion: ['1.11.5', Validators.required],
+      K8sVersion: ['1.13.4', Validators.required],
       flannelVersion: ['0.10.0', Validators.required],
       helmVersion: ['2.11.0', Validators.required],
       dockerVersion: ['17.06.0', Validators.required],
