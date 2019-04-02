e3d862a92e8c6c7be8192caade3736ff27afc95a
stgleb <glebstepanov1992@gmail.com>
2019-04-02 10:25:50 +0300

    fix

--- pkg/workflows/task.go
+++ pkg/workflows/task.go
@@ -76,7 +76,7 @@ func newTask(workflowType string, workflow Workflow, repository storage.Interfac
 		Type:   workflowType,
 		Status: statuses.Todo,
 		StepStatuses: make([]StepStatus, 0, 0),
-		
+
 		workflow:   workflow,
 		repository: repository,
 	}
@@ -88,6 +88,10 @@ func (t *Task) Run(ctx context.Context, config steps.Config, out io.WriteCloser)
 
 	if t.Status == statuses.Success {
 		errChan <- nil
+		return errChan
+	}
+
+	if len(t.workflow) == 0 {
 		return errChan
 	}
 
