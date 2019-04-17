cf500df3833daf348f6837ab905a2f89d9254e9e
Serhii Aheienko <serhii.aheienko@gmail.com>
2019-04-17 19:58:57 +0300

    Disable tests verbosity for CI

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running tests"
 
-go test -v -race -covermode=atomic -coverprofile=profile.cov ./pkg/...
+GOCACHE=off go test -race -covermode=atomic -coverprofile=profile.cov ./pkg/...
 if [ $? -eq 0 ]; then
 	echo "Tests Passed"
 else
