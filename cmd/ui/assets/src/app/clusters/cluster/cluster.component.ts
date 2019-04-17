a8da309cc5f9bf3887d859b2299a566f7298bf7a
Joe Kane <josephckane@gmail.com>
2019-04-17 05:45:46 -0500

    Task List Ordering Bug (#1376)
    * Task List Ordering Bug
    
    * order incoming tasks via concat of type and id
    
    * NOTE: this commit sorts tasks lexigraphically to squash the task jumping bug,
    which means that the tasks aren't displayed in the order they occur.
    
    * remove forced kube state

--- cmd/ui/assets/src/app/clusters/cluster/cluster.component.ts
+++ cmd/ui/assets/src/app/clusters/cluster/cluster.component.ts
@@ -191,14 +191,38 @@ export class ClusterComponent implements AfterViewInit, OnDestroy {
     a.click();
   }
 
+  orderTasks(a, b) {
+    const aSortId = (a.type + a.id).toUpperCase();
+    const bSortId = (b.type + b.id).toUpperCase();
+
+    let comparison = 0;
+    if (aSortId > bSortId) {
+      comparison = 1
+    } else if (aSortId < bSortId) {
+      comparison = -1
+    }
+
+    return comparison;
+  }
+
+  updateTaskList(tasks) {
+    const rows = [];
+    const orderedTasks = tasks.sort(this.orderTasks);
+    orderedTasks.forEach(t => {
+      if (this.expandedTaskIds.has(t.id)) {
+        t.showSteps = true;
+      }
+      rows.push(t, { detailRow: true, t });
+    });
+    this.tasks = new MatTableDataSource(rows);
+  }
+
   getKube() {
     // TODO: shameful how smart this ENTIRE component has become.
     this.subscriptions.add(observableTimer(0, 10000).pipe(
       switchMap(() => this.supergiant.Kubes.get(this.clusterId))).subscribe(
         k => {
           this.kube = k;
-          // for dev-ing
-          // this.kube.state = "prepare";
 
           switch (this.kube.state) {
             case 'operational': {
@@ -214,16 +238,7 @@ export class ClusterComponent implements AfterViewInit, OnDestroy {
               this.getKubeStatus(this.clusterId).subscribe(
                 tasks => {
                   this.setProvisioningStep(tasks);
-
-                  const rows = [];
-                  tasks.forEach(t => {
-                    if (this.expandedTaskIds.has(t.id)) {
-                      t.showSteps = true;
-                    }
-                    rows.push(t, { detailRow: true, t });
-                  });
-                  this.tasks = new MatTableDataSource(rows);
-
+                  this.updateTaskList(tasks);
                 },
                 err => console.log(err)
               );
@@ -233,16 +248,7 @@ export class ClusterComponent implements AfterViewInit, OnDestroy {
               this.getKubeStatus(this.clusterId).subscribe(
                 tasks => {
                   this.setProvisioningStep(tasks);
-
-                  const rows = [];
-                  tasks.forEach(t => {
-                    if (this.expandedTaskIds.has(t.id)) {
-                      t.showSteps = true;
-                    }
-                    rows.push(t, { detailRow: true, t });
-                  });
-                  this.tasks = new MatTableDataSource(rows);
-
+                  this.updateTaskList(tasks);
                 },
                 err => console.log(err)
               );
@@ -251,16 +257,7 @@ export class ClusterComponent implements AfterViewInit, OnDestroy {
             case 'failed': {
               this.getKubeStatus(this.clusterId).subscribe(
                 tasks => {
-
-                  const rows = [];
-                  tasks.forEach(t => {
-                    if (this.expandedTaskIds.has(t.id)) {
-                      t.showSteps = true;
-                    }
-                    rows.push(t, { detailRow: true, t });
-                  });
-                  this.tasks = new MatTableDataSource(rows);
-
+                  this.updateTaskList(tasks);
                 },
                 err => console.log(err)
               );
