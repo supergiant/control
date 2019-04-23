39679a9e7df1e7ce18375ca3d2ced9c53f63ad61
stgleb <glebstepanov1992@gmail.com>
2019-04-23 15:35:20 +0300

    add step configmap

---
+++ pkg/workflows/steps/configmap/configmap.go
@@ -0,0 +1,61 @@
+package configmap
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"io"
+	"text/template"
+
+	tm "github.com/supergiant/control/pkg/templatemanager"
+	"github.com/supergiant/control/pkg/workflows/steps"
+)
+
+const StepName = "configmap"
+
+type Step struct {
+	script *template.Template
+}
+
+func Init() {
+	tpl, err := tm.GetTemplate(StepName)
+
+	if err != nil {
+		panic(fmt.Sprintf("template %s not found", StepName))
+	}
+
+	steps.RegisterStep(StepName, New(tpl))
+}
+
+func (s *Step) Rollback(context.Context, io.Writer, *steps.Config) error {
+	return nil
+}
+func New(script *template.Template) *Step {
+	t := &Step{
+		script: script,
+	}
+
+	return t
+}
+
+func (s *Step) Run(ctx context.Context, out io.Writer, config *steps.Config) error {
+	err := steps.RunTemplate(ctx, s.script, config.Runner, out, nil)
+
+	if err != nil {
+		return errors.Wrap(err, StepName)
+	}
+
+	return nil
+}
+
+func (s *Step) Name() string {
+	return StepName
+}
+
+func (s *Step) Description() string {
+	return "create configmap for capacity service"
+}
+
+func (s *Step) Depends() []string {
+	return nil
+}
