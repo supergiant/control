e2bd787292fef2df7883c0f09294ec3628127fb5
stgleb <glebstepanov1992@gmail.com>
2019-05-14 15:32:00 +0300

    Add force delete query param

--- pkg/kube/handler.go
+++ pkg/kube/handler.go
@@ -310,7 +310,15 @@ func (h *Handler) listKubes(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) deleteKube(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	kubeID := vars["kubeID"]
+	forceDelete := false
+
 	logrus.Debugf("Delete kube %s", kubeID)
+
+	forceString := r.URL.Query().Get("force")
+
+	if forceString != "" {
+		forceDelete, _ = strconv.ParseBool(forceString)
+	}
 
 	if err := h.nodeProvisioner.Cancel(kubeID); err != nil {
 		logrus.Debugf("cancel kube tasks error %v", err)
@@ -391,6 +399,9 @@ func (h *Handler) deleteKube(w http.ResponseWriter, r *http.Request) {
 	go func(t *workflows.Task) {
 		// Update kube with deleting state
 		k.State = model.StateDeleting
+		// Append delete task ID to kube tasks so that task can be deleted too.
+		k.Tasks[workflows.DeleteTask] = []string{t.ID}
+
 		err = h.svc.Create(context.Background(), k)
 
 		if err != nil {
@@ -398,21 +409,13 @@ func (h *Handler) deleteKube(w http.ResponseWriter, r *http.Request) {
 		}
 
 		err = <-errChan
-		if err != nil {
+		if !forceDelete && err != nil {
 			return
 		}
 
 		// Clean up tasks in storage
-		err = h.deleteClusterTasks(context.Background(), kubeID)
-
-		if err != nil {
-			logrus.Errorf("error while deleting tasks %s", err)
-		}
-
-		// Finally delete cluster record from etcd
-		if err := h.svc.Delete(context.Background(), kubeID); err != nil {
-			logrus.Errorf("delete kube %s caused %v", kubeID, err)
-			return
+		if err := h.cleanUpKube(kubeID); err != nil {
+			logrus.Errorf("clean up kube %s caused %v", kubeID, err)
 		}
 	}(t)
 
@@ -807,6 +810,22 @@ func (h *Handler) deleteClusterTasks(ctx context.Context, kubeID string) error {
 			logrus.Warnf("delete task %s: %v", task.ID, err)
 			return err
 		}
+	}
+
+	return nil
+}
+
+func (h *Handler) cleanUpKube(kubeID string) error {
+	// Clean up tasks in storage
+	err := h.deleteClusterTasks(context.Background(), kubeID)
+
+	if err != nil {
+		logrus.Errorf("error while cleanup kube tasks %s", err)
+	}
+
+	// Finally delete cluster record from etcd
+	if err := h.svc.Delete(context.Background(), kubeID); err != nil {
+		return errors.Wrap(err, "cleanup kube %s caused %v")
 	}
 
 	return nil
@@ -1399,7 +1418,7 @@ func createKube(config *steps.Config, state model.KubeState, profile profile.Pro
 		Masters:                config.GetMasters(),
 		Nodes:                  config.GetNodes(),
 		Tasks: map[string][]string{
-			workflows.ImportTask:    {taskID},
+			workflows.ImportTask:       {taskID},
 			workflows.PreProvisionTask: {},
 			workflows.MasterTask:       {},
 			workflows.NodeTask:         {},
