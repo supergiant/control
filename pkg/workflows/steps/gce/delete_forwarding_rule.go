6272d32eb26756424381771469865e63cdd88f75
Gleb <glebstepanov1992@gmail.com>
2019-05-15 12:31:13 +0300

    Fix delete GCE cluster (#1442)

--- pkg/workflows/steps/gce/delete_forwarding_rule.go
+++ pkg/workflows/steps/gce/delete_forwarding_rule.go
@@ -2,8 +2,10 @@ package gce
 
 import (
 	"context"
-	"github.com/supergiant/control/pkg/clouds/gcesdk"
 	"io"
+	"time"
+
+	"github.com/supergiant/control/pkg/clouds/gcesdk"
 
 	"github.com/pkg/errors"
 
@@ -16,11 +18,16 @@ import (
 const DeleteForwardingRulesStepName = "gce_delete_forwarding_rules"
 
 type DeleteForwardingRulesStep struct {
+	attemptCount int
+	timeout      time.Duration
+
 	getComputeSvc func(context.Context, steps.GCEConfig) (*computeService, error)
 }
 
 func NewDeleteForwardingRulesStep() *DeleteForwardingRulesStep {
 	return &DeleteForwardingRulesStep{
+		attemptCount: 60,
+		timeout:      time.Second * 10,
 		getComputeSvc: func(ctx context.Context, config steps.GCEConfig) (*computeService, error) {
 			client, err := gcesdk.GetClient(ctx, config)
 
@@ -31,6 +38,9 @@ func NewDeleteForwardingRulesStep() *DeleteForwardingRulesStep {
 			return &computeService{
 				deleteForwardingRule: func(ctx context.Context, config steps.GCEConfig, forwardingRuleName string) (*compute.Operation, error) {
 					return client.ForwardingRules.Delete(config.ServiceAccount.ProjectID, config.Region, forwardingRuleName).Do()
+				},
+				getForwardingRule: func(ctx context.Context, config steps.GCEConfig, name string) (*compute.ForwardingRule, error) {
+					return client.ForwardingRules.Get(config.ServiceAccount.ProjectID, config.Region, name).Do()
 				},
 			}, nil
 		},
@@ -48,20 +58,47 @@ func (s *DeleteForwardingRulesStep) Run(ctx context.Context, output io.Writer,
 		return errors.Wrapf(err, "%s getting service caused", DeleteForwardingRulesStepName)
 	}
 
-	_, err = svc.deleteForwardingRule(ctx, config.GCEConfig, config.GCEConfig.ExternalForwardingRuleName)
+	for i := 0; i < s.attemptCount; i++ {
+		_, err = svc.deleteForwardingRule(ctx, config.GCEConfig, config.GCEConfig.ExternalForwardingRuleName)
 
-	if err != nil {
-		logrus.Errorf("Error deleting external forwarding rule  %s %v", config.GCEConfig.ExternalForwardingRuleName, err)
+		if err != nil {
+			logrus.Errorf("Error deleting external forwarding rule  %s %v", config.GCEConfig.ExternalForwardingRuleName, err)
+		}
+
+		_, err = svc.getForwardingRule(ctx, config.GCEConfig, config.GCEConfig.ExternalForwardingRuleName)
+
+		if isNotFound(err) {
+			break
+		}
+
+		logrus.Debugf("Forwarding rule %s still exists retry in %v",
+			config.GCEConfig.ExternalForwardingRuleName, s.timeout)
+		time.Sleep(s.timeout)
 	}
 
-	logrus.Debugf("Forwarding rule %s has been deleted", config.GCEConfig.ExternalForwardingRuleName)
+	logrus.Debugf("Forwarding rule %s has been deleted",
+		config.GCEConfig.ExternalForwardingRuleName)
 
-	_, err = svc.deleteForwardingRule(ctx, config.GCEConfig, config.GCEConfig.InternalForwardingRuleName)
+	for i := 0; i < s.attemptCount; i++ {
+		_, err = svc.deleteForwardingRule(ctx, config.GCEConfig, config.GCEConfig.InternalForwardingRuleName)
 
-	if err != nil {
-		logrus.Errorf("Error deleting internal forwarding rule %s rule %v", config.GCEConfig.InternalForwardingRuleName, err)
+		if err != nil {
+			logrus.Errorf("Error deleting internal forwarding rule %s rule %v", config.GCEConfig.InternalForwardingRuleName, err)
+		}
+
+		_, err = svc.getForwardingRule(ctx, config.GCEConfig, config.GCEConfig.InternalForwardingRuleName)
+
+		if isNotFound(err) {
+			break
+		}
+
+		logrus.Debugf("Forwarding rule %s still exists retry in %v",
+			config.GCEConfig.InternalForwardingRuleName, s.timeout)
+		time.Sleep(s.timeout)
 	}
-	logrus.Debugf("Forwarding rule %s has been deleted", config.GCEConfig.InternalForwardingRuleName)
+
+	logrus.Debugf("Forwarding rule %s has been deleted",
+		config.GCEConfig.InternalForwardingRuleName)
 
 	return nil
 }
