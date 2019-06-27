f40b020be8e7603d1195413ea06514d43d02adfd
Serhii Aheienko <serhii.aheienko@gmail.com>
2019-06-27 12:55:50 +0300

    Publish amazon AMI with preinstalled control service.

--- push_release.sh
+++ push_release.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+set -e
+
+err_report() {
+	echo "Error on line $1"
+}
+
+trap 'err_report $LINENO' ERR
+
 echo "building release artifacts"
 PROJECTDIR=${TRAVIS_HOME}/gopath/src/github.com/${TRAVIS_REPO_SLUG}
 echo ${PROJECTDIR}
 
 # prepare artifacts with goreleaser, results will be stored to the ./dist directory
 curl -sL https://git.io/goreleaser | bash -s -- --snapshot --skip-publish --rm-dist
+
+# build an AMI image with packer
+curl -L -o packer.zip https://releases.hashicorp.com/packer/1.4.1/packer_1.4.1_linux_amd64.zip
+unzip packer.zip
+./packer build -force build/packer.json
 
 # if a tag has alpha or beta in the name, it will be released as a pre-release.
 # if a tag does not have alpha or beta, it is pushed as a full release.
