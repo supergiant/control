343fe29d48a5aecdcae0d7c64e3d83a8e53d83e4
Gleb <glebstepanov1992@gmail.com>
2019-07-03 13:52:12 +0300

    Add k8s 1.15.0 version support (#1482)
    * add version 1.15.0
    
    * add 1.15 for ui

--- pkg/clouds/constants.go
+++ pkg/clouds/constants.go
@@ -17,7 +17,7 @@ const (
 	Unknown Name = "unknown"
 )
 
-var versions = []string{"1.11.5", "1.12.7", "1.13.7", "1.14.3"}
+var versions = []string{"1.11.5", "1.12.7", "1.13.7", "1.14.3", "1.15.0"}
 
 func ToProvider(name string) (Name, error) {
 	switch name {
