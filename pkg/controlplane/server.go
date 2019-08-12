fd5a334b19fddd843496c91f2d1e803cf3a2b260
Serhii Aheienko <serhii.aheienko@gmail.com>
2019-08-12 15:55:31 +0300

    Install cloud controller manager for DO

--- pkg/controlplane/server.go
+++ pkg/controlplane/server.go
@@ -37,6 +37,7 @@ import (
 	"github.com/supergiant/control/pkg/workflows/steps/azure"
 	"github.com/supergiant/control/pkg/workflows/steps/bootstraptoken"
 	"github.com/supergiant/control/pkg/workflows/steps/certificates"
+	"github.com/supergiant/control/pkg/workflows/steps/cloudcontroller"
 	"github.com/supergiant/control/pkg/workflows/steps/clustercheck"
 	"github.com/supergiant/control/pkg/workflows/steps/cni"
 	"github.com/supergiant/control/pkg/workflows/steps/configmap"
@@ -226,6 +227,7 @@ func configureApplication(cfg *Config) (*mux.Router, error) {
 	ssh.Init()
 	network.Init()
 	clustercheck.Init()
+	cloudcontroller.Init()
 	prometheus.Init()
 	dashboard.Init()
 	gce.Init(accountService)
